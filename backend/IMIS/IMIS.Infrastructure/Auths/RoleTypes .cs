91b9cbdc91cf64abba0af0f0bfa410c46247dbcd
Wilbur_Pimentel <wilburpimentel1907>
2025-01-24 15:51:55 +0800

    #249: Customize Identity API Endpoints - add RoleType under Auth

---
+++ backend/IMIS/IMIS.Infrastructure/Auths/RoleTypes .cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMIS.Infrastructure.Auths
+{
+    public static class RoleTypes
+    {
+        public const string Administrator = "Administrator";
+        public const string LdnaUser = "LdnaUser";
+        // Add more roles as needed
+    }
+}
