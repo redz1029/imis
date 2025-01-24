d4e9cfe9e379e1e224e514c4a0f6e23c699ebd87
Wilbur_Pimentel <wilburpimentel1907>
2025-01-24 15:49:04 +0800

    #249: Customize Identity API Endpoints - Add UserRefreshDto in UserModule

---
+++ backend/IMIS/IMIS.Presentation/UserModule/UserRefreshDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMIS.Presentation.UserModule
+{
+    public class UserRefreshDto
+    {
+        public string Id { get; set; }
+        public string AccessToken { get; set; }
+      
+
+
+
+
+    }
+}
