e11129cd08ace284350320dcfa67bbe1eec6feb6
Marc Rejohn Castillano <marcrejohncastillano@gmail.com>
2025-01-31 16:49:29 +0800

    #263 Saving Unit Tests for Auditor, Office, and Team

---
+++ backend/IMIS/ImisTest/BaseTests.cs
@@ -0,0 +1,38 @@
+﻿using DotNetEnv;
+using IMIS.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ImisTest
+{
+    public abstract class BaseTests
+    {
+        protected ServiceProvider _serviceProvider;
+        protected ImisDbContext _context;
+        protected ServiceCollection _services;
+
+        [SetUp]
+        public virtual void Setup()
+        {
+            Env.Load(AppDomain.CurrentDomain.BaseDirectory + "../../../.env");
+            _services = new ServiceCollection();
+
+            string connString = Environment.GetEnvironmentVariable("SQL_SERVER_CONN")!;
+            _services.AddDbContext<ImisDbContext>(options => options.UseSqlServer(connString));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _context.Dispose();
+            _serviceProvider.Dispose();
+        }
+
+        protected void BuildServiceProvider()
+        {
+            _serviceProvider = _services.BuildServiceProvider();
+            _context = _serviceProvider.GetRequiredService<ImisDbContext>();
+            _context.Database.EnsureCreated();
+        }
+    }
+}
