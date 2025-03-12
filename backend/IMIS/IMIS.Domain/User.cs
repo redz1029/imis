6ecb93077db1152cf5c734463156622638b3ee79
Wilbur_Pimentel <wilburpimentel1907>
2025-03-12 09:57:05 +0800

    #286: Update and add Users

---
+++ backend/IMIS/IMIS.Domain/User.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace IMIS.Domain
+{
+    public class User : IdentityUser
+    {
+        public string? FirstName { get; set; } 
+        public string? MiddleName { get; set; }
+        public string? LastName { get; set; }
+        public string? Prefix { get; set; }
+        public string? Suffix { get; set; }
+        public string? Position {  get; set; }         
+    }
+}
---
+++ backend/IMIS/IMIS.Domain/UserOffices.cs
@@ -0,0 +1,13 @@
+﻿using Base.Primitives;
+
+namespace IMIS.Domain
+{
+    public class UserOffices : Entity<int>
+    {     
+        public required string UserId { get; set; }
+        public required int OfficeId { get; set; }       
+        public required bool IsActive { get; set; }
+    }
+}
+
+
