6f0eac92173e55bd6cdd6cfd0defc300eb477406
Wilbur_Pimentel <wilburpimentel1907>
2025-03-12 10:09:24 +0800

    #286: Update Pgs UserOffice Repository and UserOffice Service Module

---
+++ backend/IMIS/IMIS.Persistence/UserOfficeModule/UserOfficeRepository.cs
@@ -0,0 +1,47 @@
+﻿using Base.Abstractions;
+using IMIS.Application.UserOfficeModule;
+using IMIS.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace IMIS.Persistence.UserOfficeModule
+{
+    public class UserOfficeRepository : BaseRepository<UserOffices, int, ImisDbContext>, IUserOfficeRepository
+    {
+        public UserOfficeRepository(ImisDbContext dbContext) : base(dbContext)
+        {
+        }
+        public async Task<IEnumerable<UserOffices>> GetAll(CancellationToken cancellationToken)
+        {
+            return await _dbContext.UserOffices
+                .Where(o => !o.IsDeleted)
+                .AsNoTracking()
+                .ToListAsync(cancellationToken)
+                .ConfigureAwait(false);
+        }
+
+        public new async Task<UserOffices> SaveOrUpdateAsync(UserOffices useroffice, CancellationToken cancellationToken)
+        {
+            if (useroffice == null) throw new ArgumentNullException(nameof(useroffice));
+
+            var existinguseroffice = await _dbContext.UserOffices
+                .FirstOrDefaultAsync(d => d.Id == useroffice.Id, cancellationToken)
+                .ConfigureAwait(false);
+
+            if (existinguseroffice != null)
+            {
+                // Update the existing record's values
+                _dbContext.Entry(existinguseroffice).CurrentValues.SetValues(useroffice);
+            }
+            else
+            {
+                // Add a new PgsAuditDetails if not found
+                await _dbContext.UserOffices.AddAsync(useroffice, cancellationToken).ConfigureAwait(false);
+            }
+
+            // Save changes to the database
+            await _dbContext.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
+            return useroffice;
+        }
+
+    }
+}
---
+++ backend/IMIS/IMIS.Persistence/UserOfficeModule/UserOfficeService.cs
@@ -0,0 +1,65 @@
+﻿using Base.Primitives;
+using IMIS.Application.PgsKraModule;
+using IMIS.Application.PGSReadinessRatingCancerCareModule;
+using IMIS.Application.UserOfficeModule;
+using IMIS.Domain;
+
+namespace IMIS.Persistence.UserOfficeModule
+{
+    public class UserOfficeService : IUserOfficeService
+    {
+        private readonly IUserOfficeRepository _repository;     
+
+        public UserOfficeService(IUserOfficeRepository repository)          
+        {
+            _repository = repository;            
+        }
+        private UserOfficeDto ConvertToDTO(UserOffices entity)
+        {
+            if (entity == null) return null;
+
+            return new UserOfficeDto
+            {
+                Id = entity.Id,
+                UserId = entity.UserId,
+                OfficeId = entity.OfficeId, 
+                IsActive = entity.IsActive
+            };
+        }
+        public async Task<UserOfficeDto?> GetByIdAsync(int id, CancellationToken cancellationToken)
+        {
+            var kradto = await _repository.GetByIdAsync(id, cancellationToken).ConfigureAwait(false);
+            return kradto != null ? ConvertToDTO(kradto) : null;
+        }
+        public async Task<List<UserOfficeDto>?> GetAllAsync(CancellationToken cancellationToken)
+        {
+            var PgsReadiness = await _repository.GetAll(cancellationToken).ConfigureAwait(false);
+            return PgsReadiness?.Select(o => ConvertToDTO(o)).ToList();
+        }
+
+        public async Task<UserOfficeDto> SaveOrUpdateAsync(UserOfficeDto user, CancellationToken cancellationToken)
+        {
+            if (user == null) throw new ArgumentNullException(nameof(user));
+
+            var pgsEntity = user.ToEntity();           
+            // Handle Save or Update
+            var createdPgs = await _repository.SaveOrUpdateAsync(pgsEntity, cancellationToken);
+
+            return new UserOfficeDto
+            {
+                Id = createdPgs.Id,
+                UserId = createdPgs.UserId,
+                OfficeId = createdPgs.OfficeId,
+                IsActive = createdPgs.IsActive,               
+            };
+        }
+        public async Task SaveOrUpdateAsync<TEntity, TId>(BaseDto<TEntity, TId> dto, CancellationToken cancellationToken) where TEntity : Entity<TId>
+        {
+            if (dto is not UserOfficeDto userOfficeDto)
+                throw new ArgumentException("Invalid DTO type", nameof(dto));
+
+            var pgsEntity = userOfficeDto.ToEntity();          
+            await _repository.SaveOrUpdateAsync(pgsEntity, cancellationToken);
+        }
+    }
+}
