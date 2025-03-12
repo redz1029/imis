3b2d06a1b4e6af175d4f93fd4b4feae88b7fc68e
Wilbur_Pimentel <wilburpimentel1907>
2025-03-12 09:48:58 +0800

    #286: Update Pgs Module to Add AuditorDto

--- backend/IMIS/IMIS.Application/OfficeModule/OfficeDto.cs
+++ backend/IMIS/IMIS.Application/OfficeModule/OfficeDto.cs
@@ -8,8 +8,7 @@ namespace IMIS.Application.OfficeModule
     {
         public string? Name { get; set; }
         public bool IsActive { get; set; }
-        public List<AuditorDto>? Auditors { get; set; }
-      
+        public List<AuditorDto>? Auditors { get; set; }      
         public override Office ToEntity()
         {
             return new Office() { Id = Id, Name = Name, IsActive = IsActive };
