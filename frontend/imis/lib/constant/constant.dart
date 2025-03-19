a79252fa8fe6ce5e1aa9572279bdde5db00205d2
Earlene Mae Rama <earlmaerama@gmail.com>
2025-03-19 16:52:13 +0800

    Constant for colors and sizing

---
+++ frontend/imis/lib/constant/constant.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color.fromRGBO(192, 74, 74, 1);
+const secondaryBgButton = Color.fromRGBO(192, 74, 74, 0.226);
+const primaryLightColor = Color.fromRGBO(192, 74, 74, 0.644);
+const secondaryColor = Color.fromRGBO(253, 252, 250, 1);
+const mainBgColor = Color.fromRGBO(245, 245, 245, 1);
+const grey = Color.fromRGBO(161, 161, 161, 1);
+const lightGrey = Color.fromRGBO(217, 217, 217, 1);
+const lightGrey1 = Color.fromRGBO(245, 245, 245, 1);
+const primaryTextColor = Color.fromRGBO(65, 65, 65, 1);
+
+const gap = SizedBox(height: 16);
+
+const gap1 = SizedBox(height: 14);
+const gap2 = SizedBox(height: 4);
+const gap3 = SizedBox(height: 32);
+// const lightGrey = Color.fromRGBO(217, 217, 217, 1);
+// const mainBgColor = Color.fromRGBO(253, 252, 250, 1);
