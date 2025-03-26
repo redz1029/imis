9b5ce4416af00e0916b99659e618472eaa326e01
Earlene Mae Rama <earlmaerama@gmail.com>
2025-03-26 13:51:13 +0800

    Change class name from pgs.dart to pgs_audit_details

--- frontend/imis/lib/performance_governance_system/models/pgs_audit_details.dart
+++ frontend/imis/lib/performance_governance_system/models/pgs_audit_details.dart
@@ -1,12 +1,13 @@
 import 'package:imis/office/models/office.dart';
+import 'package:imis/performance_governance_system/models/pgs_deliverables.dart';
 import 'package:imis/performance_governance_system/models/pgs_readiness.dart';
 import 'package:imis/performance_governance_system/pgs_period/models/pgs_period.dart';
 import 'package:json_annotation/json_annotation.dart';
 
-part 'pgs.g.dart';
+part 'pgs_audit_details.g.dart';
 
 @JsonSerializable()
-class Pgs {
+class PgsAuditDetails {
   int id;
 
   @JsonKey()
@@ -19,21 +20,26 @@ class Pgs {
   String? remarks;
 
   @JsonKey()
+  List<PgsDeliverables> pgsDeliverables;
+
+  @JsonKey()
   PgsReadiness pgsReadinessRating;
 
   bool isDeleted;
   String? rowVersion;
 
-  Pgs(
+  PgsAuditDetails(
     this.id,
     this.pgsPeriod,
     this.office,
+    this.pgsDeliverables,
     this.pgsReadinessRating,
     this.isDeleted, {
     this.remarks,
     this.rowVersion,
   });
 
-  factory Pgs.fromJson(Map<String, dynamic> json) => _$PgsFromJson(json);
-  Map<String, dynamic> toJson() => _$PgsToJson(this);
+  factory PgsAuditDetails.fromJson(Map<String, dynamic> json) =>
+      _$PgsAuditDetailsFromJson(json);
+  Map<String, dynamic> toJson() => _$PgsAuditDetailsToJson(this);
 }
