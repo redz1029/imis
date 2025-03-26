0afabc406241fe96ca9642d27f61a06496239b6c
Earlene Mae Rama <earlmaerama@gmail.com>
2025-03-26 09:47:25 +0800

    Update api endpoint

--- frontend/imis/lib/utils/api_endpoint.dart
+++ frontend/imis/lib/utils/api_endpoint.dart
@@ -8,11 +8,21 @@ class ApiEndpoint {
   late String login;
   late String register;
   late String changepassword;
+  late String keyresult;
+  late String auditor;
+  late String pgsperiod;
+  late String deliverables;
+  late String pgsauditordetails;
 
   ApiEndpoint() {
     login = '$baseUrl/login';
     register = '$baseUrl/register';
     changepassword = '$baseUrl/changePassword';
+    keyresult = '$baseUrl/Kra';
+    auditor = '$baseUrl/auditors';
+    pgsperiod = '$baseUrl/PgsPeriod';
+    deliverables = '$baseUrl/Deliverable';
+    pgsauditordetails = '$baseUrl/PgsAuditDetails';
   }
 
   static Future<void> setBaseUrl() async {
