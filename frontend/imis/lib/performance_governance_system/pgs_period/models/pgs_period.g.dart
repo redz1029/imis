896c15caa2a24ce0fab8a877292111ad8bfb4a1d
Earlene Mae Rama <earlmaerama@gmail.com>
2025-03-26 09:44:50 +0800

    Update pgs period with models

--- frontend/imis/lib/performance_governance_system/pgs_period/models/pgs_period.g.dart
+++ frontend/imis/lib/performance_governance_system/pgs_period/models/pgs_period.g.dart
@@ -8,6 +8,7 @@ part of 'pgs_period.dart';
 
 PgsPeriod _$PgsPeriodFromJson(Map<String, dynamic> json) => PgsPeriod(
   (json['id'] as num).toInt(),
+  json['isDeleted'] as bool,
   const DateTimeConverter().fromJson(json['startDate'] as String),
   const DateTimeConverter().fromJson(json['endDate'] as String),
   rowVersion: json['rowVersion'] as String?,
@@ -15,6 +16,7 @@ PgsPeriod _$PgsPeriodFromJson(Map<String, dynamic> json) => PgsPeriod(
 
 Map<String, dynamic> _$PgsPeriodToJson(PgsPeriod instance) => <String, dynamic>{
   'id': instance.id,
+  'isDeleted': instance.isDeleted,
   'startDate': const DateTimeConverter().toJson(instance.startDate),
   'endDate': const DateTimeConverter().toJson(instance.endDate),
   'rowVersion': instance.rowVersion,
