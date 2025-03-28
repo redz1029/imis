ff526f846e1c5f83a7c32bc86ddfebc272c65d53
Earlene Mae Rama <earlmaerama@gmail.com>
2025-03-28 08:58:46 +0800

    Added reusable header cell widget

--- frontend/imis/lib/constant/constant.dart
+++ frontend/imis/lib/constant/constant.dart
@@ -29,3 +29,37 @@ class Logo extends StatelessWidget {
     );
   }
 }
+
+class BuildHeaderCell extends StatelessWidget {
+  final String text;
+  final Color color;
+  final double fontSize;
+  final FontStyle fontStyle;
+
+  const BuildHeaderCell({
+    super.key,
+    required this.text,
+    this.color = Colors.black,
+    this.fontSize = 15,
+    this.fontStyle = FontStyle.normal,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Center(
+        child: Text(
+          text,
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: fontSize,
+            color: color,
+            fontStyle: fontStyle,
+          ),
+        ),
+      ),
+    );
+  }
+}
