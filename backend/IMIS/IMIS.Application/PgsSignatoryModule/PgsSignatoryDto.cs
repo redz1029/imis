87d85d28790bade2ac7cb34b4ebf4dcd63f9696d
Wilbur_Pimentel <wilburpimentel1907>
2025-05-06 08:57:38 +0800

    #393: Add Pgs Signatory PgsSignatoryDto and PgsSignatory

---
+++ backend/IMIS/IMIS.Application/PgsSignatoryModule/PgsSignatoryDto.cs
@@ -0,0 +1,25 @@
+﻿using Base.Primitives;
+using IMIS.Domain;
+
+namespace IMIS.Application.PgsSignatoryModule
+{
+    public class PgsSignatoryDto : BaseDto<PgsSignatory, long>
+    {
+        public required long PgsId { get; set; }    
+        public required int PgsSignatoryTemplateId { get; set; }     
+        public required string SignatoryId { get; set; }        
+        public DateTime DateSigned { get; set; }
+
+        public override PgsSignatory ToEntity()
+        {
+            return new PgsSignatory()
+            {
+                Id = Id,
+                PgsId = PgsId,
+                PgsSignatoryTemplateId = PgsSignatoryTemplateId,
+                SignatoryId = SignatoryId,
+                DateSigned = DateSigned
+            };
+        }
+    }
+}
---
+++ backend/IMIS/IMIS.Domain/PgsSignatory.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Base.Primitives;
+
+namespace IMIS.Domain
+{
+    public class PgsSignatory : Entity<long>
+    {
+        public required long PgsId { get; set; }
+        [ForeignKey("PgsId")]
+        public virtual PerfomanceGovernanceSystem? PerfomanceGovernanceSystem { get; set; }
+        public required int PgsSignatoryTemplateId { get; set; }
+        [ForeignKey("PgsSignatoryTemplateId")]
+        public virtual PgsSignatoryTemplate? PgsSignatoryTemplate { get; set; }
+        public required string SignatoryId { get; set; }
+        [ForeignKey("SignatoryId")]
+        public virtual User? Signatory { get; set; }
+        public DateTime DateSigned { get; set; }
+    }
+}
