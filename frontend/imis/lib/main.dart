8c2b11333a1f9b6e179ceb9e02c866567ea4dadb
Earlene Mae Rama <earlmaerama@gmail.com>
2025-05-08 16:25:28 +0800

    Removed unecessary import package in main.dart

--- frontend/imis/lib/main.dart
+++ frontend/imis/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:imis/auth/pages/login_page.dart';
-import 'package:imis/navigation/dashboard_navigation_panel.dart';
-import 'package:imis/performance_governance_system/pages/retrieve_period_page.dart';
+import 'package:imis/performance_governance_system/pages/pgs_print_page.dart';
+
 import 'package:imis/utils/api_endpoint.dart';
 
 void main() async {
