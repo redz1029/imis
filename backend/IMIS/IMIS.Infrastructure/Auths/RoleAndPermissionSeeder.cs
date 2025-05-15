bf02f01074887fabab0b4948184745f0dec5b38e
Wilbur_Pimentel <wilburpimentel1907>
2025-05-15 13:12:40 +0800

    #286: update SeedAdminRolesAndPermissionsForDefaultUsers

--- backend/IMIS/IMIS.Infrastructure/Auths/RoleAndPermissionSeeder.cs
+++ backend/IMIS/IMIS.Infrastructure/Auths/RoleAndPermissionSeeder.cs
@@ -32,7 +32,7 @@ namespace IMIS.Infrastructure.Auths
 
             // This will seed all roles and permissions for the default users
             await PermissionSeeder.SeedAdminRolesAndPermissionsForDefaultUsers<IdentityRole, User>(
-                 _roleManager, _userManager, "Wilbur", "earls", "Queen");
+                 _roleManager, _userManager, "Wilbur");
                 
 
         }
