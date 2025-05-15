4bb010605540852838fec43c4c0dbe56a47b3e0a
Wilbur_Pimentel <wilburpimentel1907>
2025-05-15 11:28:37 +0800

    #286: Add TeamPermission

---
+++ backend/IMIS/IMIS.Application/TeamModule/TeamPermission.cs
@@ -0,0 +1,10 @@
+﻿using Base.Auths.Permissions;
+
+namespace IMIS.Application.TeamModule
+{
+    public class TeamPermission : BaseOperationPermission
+    {
+        public override string ModuleName => "Team";
+        public override string PermissionGroup => PermissionGrouper.Setting;
+    }
+}
