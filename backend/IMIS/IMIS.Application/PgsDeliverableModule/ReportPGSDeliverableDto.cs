14730726c53278a8c689918f3a5d5b5aca328c50
Wilbur_Pimentel <wilburpimentel1907>
2025-05-27 16:37:47 +0800

    #390: add ReportPGSDeliverableDto

---
+++ backend/IMIS/IMIS.Application/PgsDeliverableModule/ReportPGSDeliverableDto.cs
@@ -0,0 +1,56 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Base.Primitives;
+using IMIS.Application.PgsKraModule;
+using IMIS.Domain;
+
+namespace IMIS.Application.PgsDeliverableModule
+{
+    public class ReportPGSDeliverableDto : BaseDto<PgsDeliverable, long>
+    {
+        public int? KraId { get; set; }
+        public KeyResultAreaDto? Kra { get; set; }
+        public required bool IsDirect { get; set; }
+        public required string DeliverableName { get; set; }
+        public required DateTime ByWhen { get; set; }
+        public required double PercentDeliverables { get; set; }
+        public required PgsStatus Status { get; set; }
+        public string? Remarks { get; set; }
+        public string? KraName => Kra?.Name;
+
+        public ReportPGSDeliverableDto() { }
+
+        [SetsRequiredMembers]
+        public ReportPGSDeliverableDto(PgsDeliverable pgsDeliverable)
+        {
+            this.Id = pgsDeliverable.Id;
+            this.KraId = pgsDeliverable.KraId;
+            this.IsDirect = pgsDeliverable.IsDirect;
+            this.DeliverableName = pgsDeliverable.DeliverableName;
+            this.ByWhen = pgsDeliverable.ByWhen;
+            this.PercentDeliverables = pgsDeliverable.PercentDeliverables;
+            this.Status = pgsDeliverable.Status;
+            this.Remarks = pgsDeliverable.Remarks;
+
+            if (pgsDeliverable.Kra != null)
+            {
+                this.Kra = new KeyResultAreaDto(pgsDeliverable.Kra);
+            }
+        }
+
+        public override PgsDeliverable ToEntity()
+        {
+            return new PgsDeliverable()
+            {
+                Id = Id,
+                IsDirect = IsDirect,
+                DeliverableName = DeliverableName,
+                ByWhen = ByWhen,
+                PercentDeliverables = PercentDeliverables,
+                Status = Status,
+                KraId = KraId,
+                Kra = Kra?.ToEntity(),
+                Remarks = Remarks
+            };
+        }
+    }
+}
