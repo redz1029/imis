439f711b9950b75435cb168e22ec39d521895dcf
Wilbur_Pimentel <wilburpimentel1907>
2025-05-27 16:39:11 +0800

    #390: add ReportPgsSignatoryDto

--- backend/IMIS/IMIS.Application/PgsSignatoryModule/ReportPgsSignatoryDto.cs
+++ backend/IMIS/IMIS.Application/PgsSignatoryModule/ReportPgsSignatoryDto.cs
@@ -1,17 +1,18 @@
-﻿using System.ComponentModel;
-using System.Text.Json.Serialization;
-using Base.Primitives;
+﻿using Base.Primitives;
 using IMIS.Application.PgsSignatoryTemplateModule;
 using IMIS.Domain;
 
 namespace IMIS.Application.PgsSignatoryModule
 {
-    public class PgsSignatoryDto : BaseDto<PgsSignatory, long>
+    public class ReportPgsSignatoryDto : BaseDto<PgsSignatory, long>
     {
-        public required long PgsId { get; set; }    
-        public required int PgsSignatoryTemplateId { get; set; }     
-        public required string SignatoryId { get; set; }        
-        public DateTime DateSigned { get; set; }     
+        public required long PgsId { get; set; }
+        public required int PgsSignatoryTemplateId { get; set; }
+        public required string SignatoryId { get; set; }
+        public DateTime DateSigned { get; set; }
+        public PgsSignatoryTemplateDto? PgsSignatoryTemplate { get; set; }
+        public User? User { get; set; }
+
         public override PgsSignatory ToEntity()
         {
             return new PgsSignatory()
