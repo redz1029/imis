22b60b5b8512eb4e7adc699f7a354a684e317540
Wilbur_Pimentel <wilburpimentel1907>
2025-06-10 13:37:47 +0800

    update RoleConfiguration  removed Name = RoleTypes.Administrator,  Name = RoleTypes.PgsManager,  Name = RoleTypes.PgsUser, and Name = RoleTypes.PgsAuditor replace Name = new AdministratorRole().Name,  Name = new PgsManagerRole().Name,  Name = new StandardUserRole().Name,      Name = new PgsAuditorRole().Name,

--- backend/IMIS/IMIS.Persistence/SeedConfigurations/RoleConfiguration.cs
+++ backend/IMIS/IMIS.Persistence/SeedConfigurations/RoleConfiguration.cs
@@ -2,6 +2,8 @@
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Microsoft.EntityFrameworkCore;
 using IMIS.Infrastructure.Auths;
+using Base.Auths.Roles;
+using IMIS.Infrastructure.Auths.Roles;
 
 public class RoleConfiguration : IEntityTypeConfiguration<IdentityRole>
 {
@@ -12,14 +14,14 @@ public class RoleConfiguration : IEntityTypeConfiguration<IdentityRole>
                 new IdentityRole
                 {
                     Id = "56996e97-9e8a-4d22-a693-c865144e9b96",
-                    Name = RoleTypes.Administrator,
+                    Name = new AdministratorRole().Name,
                     NormalizedName = "ADMINISTRATOR",
                     ConcurrencyStamp = Guid.NewGuid().ToString(),
                 },
                 new IdentityRole
                 {
                     Id = "8d9f58ec-a8b2-4738-9b5f-d5ce46f98b17",
-                    Name = RoleTypes.PgsManager,
+                    Name = new PgsManagerRole().Name,
                     NormalizedName = "PGS MANAGER",
                     ConcurrencyStamp = Guid.NewGuid().ToString(),
                    
@@ -27,14 +29,14 @@ public class RoleConfiguration : IEntityTypeConfiguration<IdentityRole>
                 new IdentityRole
                 {
                     Id = "f7cf5c73-16d9-4da8-9e0a-cc149b34fbbd",
-                    Name = RoleTypes.PgsUser,
+                    Name = new StandardUserRole().Name,
                     NormalizedName = "PGS USER",
                     ConcurrencyStamp = Guid.NewGuid().ToString(),
                 },
                  new IdentityRole
                  {
                      Id = "af7b586c7ee6490bbd878f46f6a47831",
-                     Name = RoleTypes.PgsAuditor,
+                     Name = new PgsAuditorRole().Name,
                      NormalizedName = "PGS AUDITOR",
                      ConcurrencyStamp = Guid.NewGuid().ToString(),
                  }
