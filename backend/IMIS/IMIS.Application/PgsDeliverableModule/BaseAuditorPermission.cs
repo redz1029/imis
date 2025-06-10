aca411fcee5f367edc0afa4a0e77dd785cbaf269
Wilbur_Pimentel <wilburpimentel1907>
2025-06-10 13:32:07 +0800

    update pgs deliverable module add BaseAuditorPermission and PgsDeliverableAuditorPermission

---
+++ backend/IMIS/IMIS.Application/PgsDeliverableModule/BaseAuditorPermission.cs
@@ -0,0 +1,11 @@
+﻿using Base.Auths.Permissions;
+
+namespace IMIS.Application.PgsDeliverableModule
+{
+    public abstract class BaseAuditorPermission : BasePermission
+    {
+        // Permissions dynamically generated based on the module name    
+        public string View => $"View{ModuleName}";
+        public string Score => $"Score{ModuleName}";
+    }
+}
---
+++ backend/IMIS/IMIS.Application/PgsDeliverableModule/PgsDeliverableAuditorPermission.cs
@@ -0,0 +1,9 @@
+﻿
+namespace IMIS.Application.PgsDeliverableModule
+{
+    public class PgsDeliverableAuditorPermission : BaseAuditorPermission
+    {
+        public override string ModuleName => "PgsDeliverableMonitor";
+        public override string PermissionGroup => PermissionGrouper.Report;
+    }
+}
