2177dc91c22021c6887c06894ee446a0a58c63e6
Wilbur_Pimentel <wilburpimentel1907>
2025-06-16 09:13:13 +0800

    add permissions

---
+++ backend/IMIS/IMIS.Application/PgsDeliverableModule/PgsDeliverablePermission.cs
@@ -0,0 +1,11 @@
+﻿using Base.Auths.Permissions;
+
+namespace IMIS.Application.PgsDeliverableModule
+{
+    public class PgsDeliverablePermission : BaseOperationPermission
+    {
+        public override string ModuleName => "PgsDeliverable";
+        public override string PermissionGroup => PermissionGrouper.Setting;
+
+    }
+}
