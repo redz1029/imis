88fef6dbdc067891ff788162502c78637d423074
Wilbur_Pimentel <wilburpimentel1907>
2025-06-18 09:49:49 +0800

    update AuditSchedule add _auditSchedulePermission

---
+++ backend/IMIS/IMIS.Application/AuditScheduleModule/AuditSchedulePermission.cs
@@ -0,0 +1,10 @@
+﻿using Base.Auths.Permissions;
+
+namespace IMIS.Application.AuditScheduleModule
+{
+    public class AuditSchedulePermission : BaseOperationPermission
+    {
+        public override string ModuleName => "AuditSchedule";
+        public override string PermissionGroup => PermissionGrouper.Setting;
+    }
+}
--- backend/IMIS/IMIS.Presentation/AuditScheduleModule/AuditScheduleEndPoints.cs
+++ backend/IMIS/IMIS.Presentation/AuditScheduleModule/AuditScheduleEndPoints.cs
@@ -1,6 +1,8 @@
-﻿using Carter;
+﻿using Base.Auths.Permissions;
+using Carter;
 using IMIS.Application.AuditableOfficesModule;
 using IMIS.Application.AuditScheduleModule;
+using IMIS.Application.OfficeModule;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -13,6 +15,7 @@ namespace IMIS.Presentation.AuditScheduleModule
     public class AuditScheduleEndPoints : CarterModule
     {
         private const string _AuditSchedule = "AuditSchedule";
+        public readonly AuditSchedulePermission _auditSchedulePermission = new();
         public AuditScheduleEndPoints() : base("/auditSchedule")
         {
         }
@@ -48,28 +51,39 @@ namespace IMIS.Presentation.AuditScheduleModule
                 await cache.EvictByTagAsync(_AuditSchedule, cancellationToken);
                 return Results.Created($"/auditSchedule/{createdAuditSchedule.Id}", createdAuditSchedule);
             })
-            .WithTags(_AuditSchedule);
+            .WithTags(_AuditSchedule)
+            .RequireAuthorization(e => e.RequireClaim(
+             PermissionClaimType.Claim, _auditSchedulePermission.Add));
+
             app.MapGet("/", async (IAuditScheduleService service, CancellationToken cancellationToken) =>
             {
                 var auditScheduleDto = await service.GetAllAsync(cancellationToken).ConfigureAwait(false);
                 return Results.Ok(auditScheduleDto);
             })
            .WithTags(_AuditSchedule)
-           .CacheOutput(builder => builder.Expire(TimeSpan.FromMinutes(2)).Tag(_AuditSchedule), true);
+           .CacheOutput(builder => builder.Expire(TimeSpan.FromMinutes(2)).Tag(_AuditSchedule), true)
+           .RequireAuthorization(e => e.RequireClaim(
+            PermissionClaimType.Claim, _auditSchedulePermission.View));
+
             app.MapGet("/{id}", async (int id, IAuditScheduleService service, CancellationToken cancellationToken) =>
             {
                 var auditScheduleDto = await service.GetByIdAsync(id, cancellationToken).ConfigureAwait(false);
                 return auditScheduleDto != null ? Results.Ok(auditScheduleDto) : Results.NotFound();
             })
            .WithTags(_AuditSchedule)
-           .CacheOutput(builder => builder.Expire(TimeSpan.FromMinutes(2)).Tag(_AuditSchedule), true);
+           .CacheOutput(builder => builder.Expire(TimeSpan.FromMinutes(2)).Tag(_AuditSchedule), true)
+           .RequireAuthorization(e => e.RequireClaim(
+            PermissionClaimType.Claim, _auditSchedulePermission.Edit));
+
             app.MapGet("/page", async (int page, int pageSize, IAuditScheduleService service, CancellationToken cancellationToken) =>
             {
                 var paginatedAuditSchedule = await service.GetPaginatedAsync(page, pageSize, cancellationToken).ConfigureAwait(false);
                 return Results.Ok(paginatedAuditSchedule);
             })
            .WithTags(_AuditSchedule)
-           .CacheOutput(builder => builder.Expire(TimeSpan.FromMinutes(2)).Tag(_AuditSchedule), true);
+           .CacheOutput(builder => builder.Expire(TimeSpan.FromMinutes(2)).Tag(_AuditSchedule), true)
+           .RequireAuthorization(e => e.RequireClaim(
+            PermissionClaimType.Claim, _auditSchedulePermission.View));
         }
     }
 }
