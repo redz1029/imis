715a94c231ece235a23eff4bac9a89869bf7e153
Wilbur_Pimentel <wilburpimentel1907>
2025-06-18 09:12:48 +0800

    update UserOfficePermission

---
+++ backend/IMIS/IMIS.Application/PgsSignatoryTemplateModule/PgsSignatoryTemplatePermission.cs
@@ -0,0 +1,10 @@
+﻿using Base.Auths.Permissions;
+
+namespace IMIS.Application.PgsSignatoryTemplateModule
+{
+    public class PgsSignatoryTemplatePermission : BaseOperationPermission
+    {
+        public override string ModuleName => "PgsSignatoryTemplate";
+        public override string PermissionGroup => PermissionGrouper.Setting;
+    }
+}
