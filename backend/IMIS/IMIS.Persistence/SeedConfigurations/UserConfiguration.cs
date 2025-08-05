164d7dcf8dfcf117838c1eeeb57b6163e63417e2
Marc Rejohn Castillano <marcrejohncastillano@gmail.com>
2025-08-06 00:54:22 +0800

    Fixed User Seed Configuration Discriminator

--- backend/IMIS/IMIS.Persistence/SeedConfigurations/UserConfiguration.cs
+++ backend/IMIS/IMIS.Persistence/SeedConfigurations/UserConfiguration.cs
@@ -1,19 +1,20 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using IMIS.Domain;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 
-public class UserConfiguration : IEntityTypeConfiguration<IdentityUser>
+public class UserConfiguration : IEntityTypeConfiguration<User>
 {
     
-    public void Configure(EntityTypeBuilder<IdentityUser> builder)
+    public void Configure(EntityTypeBuilder<User> builder)
     {
  
         var hasher = new PasswordHasher<IdentityUser>();
         string defaultPassword = "Abc123*@";
 
         builder.HasData(
-            new IdentityUser
+            new User
             {
                 Id = "f7cf5c73-16d9-4da8-9e0a-cc149b34fbbd",             
                 UserName = "redz1029",
@@ -23,7 +24,7 @@ public class UserConfiguration : IEntityTypeConfiguration<IdentityUser>
                 PasswordHash = hasher.HashPassword(null, defaultPassword),
                 ConcurrencyStamp = Guid.NewGuid().ToString(),
             },
-            new IdentityUser
+            new User
             {
                 Id = "176bcfeb-f12a-4d42-b790-5d2312660801",                
                 UserName = "alyxx1988",
@@ -33,7 +34,7 @@ public class UserConfiguration : IEntityTypeConfiguration<IdentityUser>
                 PasswordHash = hasher.HashPassword(null, defaultPassword),
                 ConcurrencyStamp = Guid.NewGuid().ToString(),
             },
-            new IdentityUser
+            new User
             {
                 Id = "fb385d60-eaee-4ea2-8bf1-b5cc0723c17a",              
                 UserName = "darkwizwiz",
