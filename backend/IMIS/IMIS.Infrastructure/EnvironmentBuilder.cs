2b7326c8935ad3ad7bd8aeed228b12e84ca2045b
Marc Rejohn Castillano <marcrejohncastillano@gmail.com>
2025-08-06 14:54:20 +0800

    #795: Created environment builder

---
+++ backend/IMIS/IMIS.Infrastructure/EnvironmentBuilder.cs
@@ -0,0 +1,51 @@
+﻿using Base.Auths;
+using DotNetEnv;
+using IMIS.Infrastructure.Auths;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+
+namespace IMIS.Infrastructure
+{
+    public static class EnvironmentBuilder
+    {
+        public const string _sqlServerConn = "SQL_SERVER_CONN";
+        public const string _jwtIssuer = "JWT_ISSUER";
+        public const string _jwtAudience = "JWT_AUDIENCE";
+        public const string _jwtSecret = "JWT_SECRET_KEY";
+        public const string _jwtExpMinutes = "JWT_EXP_IN_MINUTES";
+        public const string _jwtExpDays = "JWT_EXP_IN_DAYS";
+
+        public static void SetupEnvironment(this WebApplicationBuilder builder)
+        {
+            if (builder.Environment.IsDevelopment())
+            {
+                Env.Load();
+
+                DatabaseCredentials.SqlServerConnectionString = Environment.GetEnvironmentVariable(_sqlServerConn);
+
+                TokenUtils.Issuer = Environment.GetEnvironmentVariable(_jwtIssuer);
+                TokenUtils.Audience = Environment.GetEnvironmentVariable(_jwtAudience);
+                TokenUtils.SecretKey = Environment.GetEnvironmentVariable(_jwtSecret);
+                TokenUtils.ExpInMinutes = Environment.GetEnvironmentVariable(_jwtExpMinutes);
+                TokenUtils.ExpInDays = Environment.GetEnvironmentVariable(_jwtExpDays);
+            }
+            else
+            {
+                string tokenPath = "path-of-vault-token-in-server";
+                string vaultUrl = "http://vault-url-here";
+                VaultService vaultService = new(tokenPath, vaultUrl);
+
+                string mounthPath = "imis-demo";
+                var secretDbCreds = vaultService.GetSecret(mounthPath, "db-credentials");
+                DatabaseCredentials.SqlServerConnectionString = vaultService.GetSecretValue(secretDbCreds, _sqlServerConn);
+
+                var secretJwtCreds = vaultService.GetSecret(mounthPath, "jwt-config");
+                TokenUtils.Issuer = vaultService.GetSecretValue(secretJwtCreds, _jwtIssuer);
+                TokenUtils.Audience = vaultService.GetSecretValue(secretJwtCreds, _jwtAudience);
+                TokenUtils.SecretKey = vaultService.GetSecretValue(secretJwtCreds, _jwtSecret);
+                TokenUtils.ExpInMinutes = vaultService.GetSecretValue(secretJwtCreds, _jwtExpMinutes);
+                TokenUtils.ExpInDays = vaultService.GetSecretValue(secretJwtCreds, _jwtExpDays);
+            }
+        }
+    }
+}
