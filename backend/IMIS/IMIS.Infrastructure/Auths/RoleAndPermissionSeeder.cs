91ad1de1d20b3c09adc308d0b28e5f81c246cea9
Wilbur_Pimentel <wilburpimentel1907>
2025-08-11 16:41:57 +0800

    update RoleAndPermissionSeeder replace "castillano.mrb" with "Wilbur"

--- backend/IMIS/IMIS.Infrastructure/Auths/RoleAndPermissionSeeder.cs
+++ backend/IMIS/IMIS.Infrastructure/Auths/RoleAndPermissionSeeder.cs
@@ -50,7 +50,7 @@ namespace IMIS.Infrastructure.Auths
 
             // This will seed all roles and permissions for the default users
             await PermissionSeeder.SeedAdminRolesAndPermissionsForDefaultUsers<IdentityRole, User>(             
-                 _roleManager, _userManager, "castillano.mrb");
+                 _roleManager, _userManager, "Wilbur");
                 
 
         }
