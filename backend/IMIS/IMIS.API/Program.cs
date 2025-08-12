511f53138792979e4a9249a6998be0d012fcaf00
Marc Rejohn Castillano <marcrejohncastillano@gmail.com>
2025-08-12 09:36:10 +0800

    #795: Created environment builder

--- backend/IMIS/IMIS.API/Program.cs
+++ backend/IMIS/IMIS.API/Program.cs
@@ -15,10 +15,11 @@ using System.Text;
 using Base.Auths.Permissions;
 using Base.Auths.Roles;
 using IMIS.Infrastructure.Auths.Roles;
+using IMIS.Infrastructure;
 
 
 var builder = WebApplication.CreateBuilder(args);
-Env.Load();
+builder.SetupEnvironment();
 
 var allowedOrigins = "_allowedOrigins";
 
@@ -30,27 +31,6 @@ builder.Services.AddCors(opts => opts.AddPolicy(allowedOrigins, policy =>
 }));
 
 builder.Services.AddCarter();
-
-var sqlServerConnectionString = Environment.GetEnvironmentVariable("SQL_SERVER_CONN")
-    ?? throw new InvalidOperationException("SQL_SERVER_CONN environment variable is not set or empty.");
-
-TokenUtils.Issuer = Environment.GetEnvironmentVariable("JWT_ISSUER")
-    ?? throw new InvalidOperationException("JWT_ISSUER environment variable is not set or empty.");
-
-TokenUtils.Audience = Environment.GetEnvironmentVariable("JWT_AUDIENCE")
-    ?? throw new InvalidOperationException("JWT_AUDIENCE environment variable is not set or empty.");
-
-TokenUtils.SecretKey = Environment.GetEnvironmentVariable("JWT_SECRET_KEY")
-    ?? throw new InvalidOperationException("JWT_SECRET_KEY environment variable is not set or empty.");
-
-TokenUtils.ExpInMinutes = Environment.GetEnvironmentVariable("JWT_EXP_IN_MINUTES")
-    ?? throw new InvalidOperationException("JWT_EXP_IN_MINUTES environment variable is not set or empty.");
-
-TokenUtils.ExpInDays = Environment.GetEnvironmentVariable("JWT_EXP_IN_DAYS")
-    ?? throw new InvalidOperationException("JWT_EXP_IN_DAYS environment variable is not set or empty.");
-
-Console.WriteLine($"Issuer: {TokenUtils.Issuer}, Audience: {TokenUtils.Audience}, SecretKey Length: {TokenUtils.SecretKey.Length}");
-
 
 builder.Services.AddAuthentication(options =>
 {
