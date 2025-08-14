428a2bb0476533422c0fd13a17607ab8edaf02e9
Wilbur_Pimentel <wilburpimentel1907>
2025-08-14 16:21:26 +0800

    removed unused imports

--- backend/IMIS/IMIS.Application/OfficeTypeModule/OfficeTypePermission.cs
+++ backend/IMIS/IMIS.Application/OfficeTypeModule/OfficeTypePermission.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Base.Auths.Permissions;
+﻿using Base.Auths.Permissions;
 
 namespace IMIS.Application.OfficeTypeModule
 {
