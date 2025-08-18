5645fcd53009f9b8112eb71b5b23ee564e248bf2
Wilbur_Pimentel <wilburpimentel1907>
2025-08-18 18:42:34 +0800

    update pgs report

--- backend/IMIS/IMIS.Infrastructure/Auths/RoleAndPermissionSeeder.cs
+++ backend/IMIS/IMIS.Infrastructure/Auths/RoleAndPermissionSeeder.cs
@@ -54,7 +54,8 @@ namespace IMIS.Infrastructure.Auths
 
             // This will seed all roles and permissions for the default users
             await PermissionSeeder.SeedAdminRolesAndPermissionsForDefaultUsers<IdentityRole, User>(
-                 _roleManager, _userManager, "castillano.mrb", "rama.emg", "pimentel.wb");
+                 _roleManager, _userManager, "Admin");
+                 //_roleManager, _userManager, "castillano.mrb", "rama.emg", "pimentel.wb");
                 
 
         }
