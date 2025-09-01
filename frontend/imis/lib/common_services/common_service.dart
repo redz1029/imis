3a77599fe1ef9703e445933e39a97d5fa481798a
Earlene Mae Rama <earlmaerama@gmail.com>
2025-09-01 13:41:08 +0800

    Create common service to centralized reusable api alls

---
+++ frontend/imis/lib/common_services/common_service.dart
@@ -0,0 +1,35 @@
+import 'package:dio/dio.dart';
+import 'package:imis/office/models/office.dart';
+import 'package:imis/user/models/user.dart';
+import 'package:imis/utils/api_endpoint.dart';
+import 'package:imis/utils/http_util.dart';
+
+class CommonService {
+  final Dio dio;
+
+  CommonService(this.dio);
+
+  Future<List<Office>> fetchOffices() async {
+    var url = ApiEndpoint().office;
+
+    final response = await AuthenticatedRequest.get(dio, url);
+
+    if (response.statusCode == 200 && response.data is List) {
+      return (response.data as List).map((e) => Office.fromJson(e)).toList();
+    } else {
+      throw Exception('Failed to fetch offices');
+    }
+  }
+
+  Future<List<User>> fetchUsers() async {
+    var url = ApiEndpoint().users;
+
+    final response = await AuthenticatedRequest.get(dio, url);
+
+    if (response.statusCode == 200 && response.data is List) {
+      return (response.data as List).map((e) => User.fromJson(e)).toList();
+    } else {
+      throw Exception('Failed to fetch users');
+    }
+  }
+}
