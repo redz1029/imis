9333c939d70e6464d79f8375083770e7092cbef8
Earlene Mae Rama <earlmaerama@gmail.com>
2025-09-01 13:41:27 +0800

    Create user office service

---
+++ frontend/imis/lib/user/services/user_office_service.dart
@@ -0,0 +1,50 @@
+import 'package:dio/dio.dart';
+import 'package:imis/user/models/user_office.dart';
+import 'package:imis/utils/api_endpoint.dart';
+import 'package:imis/utils/http_util.dart';
+import 'package:imis/utils/page_list.dart';
+import 'package:imis/utils/pagination_util.dart';
+
+class UserOfficeService {
+  final Dio dio;
+  UserOfficeService(this.dio);
+
+  Future<PageList<UserOffice>> getPgsPeriod({
+    int page = 1,
+    int pageSize = 15,
+    String? searchQuery,
+  }) async {
+    final paginationUtil = PaginationUtil(dio);
+    return await paginationUtil.fetchPaginatedData(
+      endpoint: ApiEndpoint().useroffice,
+      page: page,
+      pageSize: pageSize,
+      searchQuery: searchQuery,
+      fromJson: (json) => UserOffice.fromJson(json),
+    );
+  }
+
+  Future<void> addOrUpdateUserOffice(UserOffice userOffice) async {
+    final url = ApiEndpoint().useroffice;
+    final isUpdating = userOffice.id != 0;
+    final Map<String, dynamic> requestData = userOffice.toJson();
+
+    final response =
+        isUpdating
+            ? await AuthenticatedRequest.put(
+              dio,
+              '$url/${userOffice.id}',
+              data: requestData,
+            )
+            : await AuthenticatedRequest.post(dio, url, data: requestData);
+
+    if (response.statusCode != 200 && response.statusCode != 201) {
+      throw Exception('Failed to create/update user office');
+    }
+  }
+
+  Future<void> deleteUserOffice(String teamId) async {
+    final url = '${ApiEndpoint().team}/$teamId';
+    await AuthenticatedRequest.delete(dio, url);
+  }
+}
