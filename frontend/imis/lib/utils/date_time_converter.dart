f1b24118be19b0e434f6877e888a9beeb7e7f952
Earlene Mae Rama <earlmaerama@gmail.com>
2025-09-02 11:57:58 +0800

    remove readable date/time converter class and move the codes in data/time converter

--- frontend/imis/lib/utils/date_time_converter.dart
+++ frontend/imis/lib/utils/date_time_converter.dart
@@ -12,3 +12,31 @@ class DateTimeConverter implements JsonConverter<DateTime, String> {
   @override
   String toJson(DateTime object) => dateFormat.format(object);
 }
+
+class ReadableDateTimeConverter implements JsonConverter<DateTime, String> {
+  const ReadableDateTimeConverter();
+
+  static final DateFormat _formatter = DateFormat('MMM, dd, yyyy hh:mm a');
+
+  @override
+  DateTime fromJson(String json) => _formatter.parse(json);
+
+  @override
+  String toJson(DateTime object) => _formatter.format(object);
+}
+
+class LongDateOnlyConverter implements JsonConverter<DateTime, String> {
+  const LongDateOnlyConverter();
+
+  static final DateFormat _formatter = DateFormat('MMMM dd, yyyy');
+
+  @override
+  DateTime fromJson(String json) {
+    return DateTime.parse(json);
+  }
+
+  @override
+  String toJson(DateTime object) {
+    return _formatter.format(object);
+  }
+}
