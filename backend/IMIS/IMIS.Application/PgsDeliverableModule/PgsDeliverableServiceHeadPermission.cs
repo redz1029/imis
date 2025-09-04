bb2f6d8dc64a1e7765a6516e0d37ca3d38400e23
Wilbur_Pimentel <wilburpimentel1907>
2025-09-04 18:09:49 +0800

    Add permission for service Head

---
+++ backend/IMIS/IMIS.Application/PgsDeliverableModule/PgsDeliverableServiceHeadPermission.cs
@@ -0,0 +1,10 @@
+﻿using Base.Auths.Permissions;
+
+namespace IMIS.Application.PgsDeliverableModule
+{
+    public class PgsDeliverableServiceHeadPermission : BaseOperationPermission
+    {
+        public override string ModuleName => "PgsDeliverableServiceHead";
+        public override string PermissionGroup => PermissionGrouper.Report;
+    }
+}
