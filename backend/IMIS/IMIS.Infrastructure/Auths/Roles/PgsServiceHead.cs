c5737d6a9f831db925ae9abe72742673257d83ea
Wilbur_Pimentel <wilburpimentel1907>
2025-09-04 18:12:45 +0800

    add service head from infrastructure

---
+++ backend/IMIS/IMIS.Infrastructure/Auths/Roles/PgsServiceHead.cs
@@ -0,0 +1,9 @@
+﻿using Base.Auths.Roles;
+
+namespace IMIS.Infrastructure.Auths.Roles
+{
+    public class PgsServiceHead : BaseRole
+    {
+        public override string Name => "PGS Service Head";
+    }
+}
