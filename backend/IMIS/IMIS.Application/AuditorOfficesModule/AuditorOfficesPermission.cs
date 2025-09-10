07d69dc43015708c37762c8b2a5c7188c19052a7
Wilbur_Pimentel <wilburpimentel1907>
2025-09-10 09:02:22 +0800

    Create Auditor Office Permission

---
+++ backend/IMIS/IMIS.Application/AuditorOfficesModule/AuditorOfficesPermission.cs
@@ -0,0 +1,10 @@
+﻿using Base.Auths.Permissions;
+
+namespace IMIS.Application.AuditorOfficesModule
+{
+    public class AuditorOfficesPermission : BaseOperationPermission
+    {
+        public override string ModuleName => "AuditorOffices";
+        public override string PermissionGroup => PermissionGrouper.Setting;
+    }
+}
