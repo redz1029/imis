377ee1227b1ae7bd6f72f869ad9d07c3c91d232f
Wilbur_Pimentel <wilburpimentel1907>
2025-09-10 09:06:03 +0800

    Update Auditor Office Domain: Add PgsPeriod

--- backend/IMIS/IMIS.Domain/AuditorOffices.cs
+++ backend/IMIS/IMIS.Domain/AuditorOffices.cs
@@ -1,11 +1,15 @@
-﻿namespace IMIS.Domain
+﻿using Base.Primitives;
+
+namespace IMIS.Domain
 {
-    public class AuditorOffices
+    public class AuditorOffices : Entity<int>
     {
         public int AuditorId { get; set; }
         public Auditor? Auditor { get; set; }
         public int OfficeId { get; set; }
         public Office? Office { get; set; }
+        public int PgsPeriodId { get; set; }
+        public PgsPeriod? PgsPeriod { get; set; }
         public bool IsOfficeHead { get; set; }
     }
 }
