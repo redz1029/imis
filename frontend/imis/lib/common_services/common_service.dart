b8c8ac4123e5a9c4578a871a8a51cff069c8cfd6
Earlene Mae Rama <earlmaerama@gmail.com>
2025-09-11 15:45:55 +0800

    Add pgs period api calls on common service

--- frontend/imis/lib/common_services/common_service.dart
+++ frontend/imis/lib/common_services/common_service.dart
@@ -1,5 +1,7 @@
 import 'package:dio/dio.dart';
+import 'package:imis/auditor/models/auditor.dart';
 import 'package:imis/office/models/office.dart';
+import 'package:imis/performance_governance_system/pgs_period/models/pgs_period.dart';
 import 'package:imis/user/models/user.dart';
 import 'package:imis/utils/api_endpoint.dart';
 import 'package:imis/utils/http_util.dart';
@@ -30,6 +32,30 @@ class CommonService {
       return (response.data as List).map((e) => User.fromJson(e)).toList();
     } else {
       throw Exception('Failed to fetch users');
+    }
+  }
+
+  Future<List<Auditor>> fetchAuditors() async {
+    var url = ApiEndpoint().auditor;
+
+    final response = await AuthenticatedRequest.get(dio, url);
+
+    if (response.statusCode == 200 && response.data is List) {
+      return (response.data as List).map((e) => Auditor.fromJson(e)).toList();
+    } else {
+      throw Exception('Failed to fetch auditor');
+    }
+  }
+
+  Future<List<PgsPeriod>> fetchPgsPeriod() async {
+    var url = ApiEndpoint().pgsperiod;
+
+    final response = await AuthenticatedRequest.get(dio, url);
+
+    if (response.statusCode == 200 && response.data is List) {
+      return (response.data as List).map((e) => PgsPeriod.fromJson(e)).toList();
+    } else {
+      throw Exception('Failed to fetch auditor');
     }
   }
 }
