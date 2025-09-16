3e4b2f72497638464f25e3090a8616f32d6e48e2
Wilbur_Pimentel <wilburpimentel1907>
2025-09-16 09:22:40 +0800

    Add pgsStatus from PerfromanceGovernanceSystem Application:

---
+++ backend/IMIS/IMIS.Application/PerfomanceGovernanceSystemModule/PgsStatus.cs
@@ -0,0 +1,11 @@
+﻿namespace IMIS.Application.PerfomanceGovernanceSystemModule
+{
+    public class PgsStatus
+    {
+        public const string Prepared = "Prepared";
+        public const string Pending = "Pending";
+        public const string Approved = "Approved";
+        public const string Disapproved = "Disapproved";
+        public const string PreparedBy = "Prepared By";
+    }
+}
