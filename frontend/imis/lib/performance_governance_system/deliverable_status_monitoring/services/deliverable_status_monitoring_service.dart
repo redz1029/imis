2b4811a574346066f3e51607fba8c4302bc739c8
Earlene Mae Rama <earlmaerama@gmail.com>
2025-09-18 16:34:53 +0800

    Change class name

---
+++ frontend/imis/lib/performance_governance_system/deliverable_status_monitoring/services/deliverable_status_monitoring_service.dart
@@ -0,0 +1,26 @@
+import 'package:dio/dio.dart';
+import 'package:imis/office/models/office.dart';
+import 'package:imis/utils/api_endpoint.dart';
+import 'package:imis/utils/http_util.dart';
+
+class DeliverableStatusMonitoringService {
+  final Dio dio;
+
+  DeliverableStatusMonitoringService(this.dio);
+  Future<void> saveData(List<dynamic> items) async {
+    final url = ApiEndpoint().pgsScoreMonitoring;
+    await AuthenticatedRequest.put(dio, url, data: {'items': items});
+  }
+
+  Future<List<Office>> fetchOffices() async {
+    var url = ApiEndpoint().officePgsAuditor;
+
+    final response = await AuthenticatedRequest.get(dio, url);
+
+    if (response.statusCode == 200 && response.data is List) {
+      return (response.data as List).map((e) => Office.fromJson(e)).toList();
+    } else {
+      throw Exception('Failed to fetch offices');
+    }
+  }
+}
---
+++ frontend/imis/lib/reports/pages/view_summary_narrative_report_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ViewSummaryNarrativeReportPage extends StatefulWidget {
+  const ViewSummaryNarrativeReportPage({super.key});
+
+  @override
+  ViewSummaryNarrativeReportPageState createState() =>
+      ViewSummaryNarrativeReportPageState();
+}
+
+class ViewSummaryNarrativeReportPageState
+    extends State<ViewSummaryNarrativeReportPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
