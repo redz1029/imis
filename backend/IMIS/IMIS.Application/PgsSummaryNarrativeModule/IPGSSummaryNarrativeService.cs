52de6983ae2c5ab0ffad1e5902ec9fc436020ab9
Wilbur_Pimentel <wilburpimentel1907>
2025-09-19 08:27:40 +0800

    Create IPGSSummaryNarrativeService from Application/PgsSummaryNarrativeModule * GetAllAsync * GetByPeriodIdAsync * ReportGetByFilterAsync

---
+++ backend/IMIS/IMIS.Application/PgsSummaryNarrativeModule/IPGSSummaryNarrativeService.cs
@@ -0,0 +1,12 @@
+﻿using Base.Abstractions;
+using IMIS.Application.PgsSummaryNarrativeModules;
+
+namespace IMIS.Application.PgsSummaryNarrativeModule
+{
+    public interface IPGSSummaryNarrativeService : IService
+    {
+        Task<List<PGSSummaryNarrativeDto>?> GetAllAsync(CancellationToken cancellationToken);       
+        Task<PGSSummaryNarrativeDto?> GetByPeriodIdAsync(int pgsPeriodId, CancellationToken cancellationToken);
+        Task<List<ReportPGSSummaryNarrativeDto>> ReportGetByFilterAsync(PgsDeliverableSummaryNarrativeFilter filter, CancellationToken cancellationToken);
+    }
+}
