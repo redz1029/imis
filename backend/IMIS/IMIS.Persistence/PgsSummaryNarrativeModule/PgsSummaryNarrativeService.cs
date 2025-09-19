77632c121d9e6f917264245e7e4ff298343464a0
Wilbur_Pimentel <wilburpimentel1907>
2025-09-19 08:50:02 +0800

    Create PgsSummaryNarrativeService from Persistence/PgsSummaryNarrativeModule * ReportGetByFilterAsync * GetByPeriodIdAsync * GetFilteredDeliverablesAsync * GetByPeriodIdAsync * GetAllAsync * SaveOrUpdateAsync

---
+++ backend/IMIS/IMIS.Persistence/PgsSummaryNarrativeModule/PgsSummaryNarrativeService.cs
@@ -0,0 +1,98 @@
+﻿using Base.Primitives;
+using IMIS.Application.OfficeModule;
+using IMIS.Application.PgsKraModule;
+using IMIS.Application.PgsSummaryNarrativeModule;
+using IMIS.Application.PgsSummaryNarrativeModules;
+using Sprache;
+
+
+namespace IMIS.Persistence.PgsSummaryNarrativeModule
+{
+    public class PgsSummaryNarrativeService : IPGSSummaryNarrativeService
+    {
+        private readonly IPGSSummaryNarrativeRepository _repository;
+        private readonly IOfficeRepository _officeRepository;
+        private readonly IKeyResultAreaRepository _keyResulktAreaRepository;
+
+        public PgsSummaryNarrativeService(
+            IPGSSummaryNarrativeRepository repository,
+            IOfficeRepository officeRepository,
+            IKeyResultAreaRepository keyResulktAreaRepository)
+        {
+            _repository = repository;
+            _officeRepository = officeRepository;
+            _keyResulktAreaRepository = keyResulktAreaRepository;
+        }       
+        public async Task<List<ReportPGSSummaryNarrativeDto>> ReportGetByFilterAsync(PgsDeliverableSummaryNarrativeFilter filter, CancellationToken cancellationToken)
+        {
+            var narratives = await _repository.GetNarrativesByFilterAsync(filter, cancellationToken);
+            var periodIds = narratives
+                .Where(n => n.PgsPeriodId.HasValue)
+                .Select(n => n.PgsPeriodId!.Value)
+                .ToList();
+
+            var deliverables = await _repository.GetFilteredDeliverablesAsync(periodIds, filter, cancellationToken);
+           
+            OfficeDto? officeDto = null;
+            if (!string.IsNullOrEmpty(filter.OfficeId))
+            {
+                var office = await _officeRepository.GetByIdAsync(int.Parse(filter.OfficeId), cancellationToken);
+                if (office != null)
+                    officeDto = new OfficeDto(office);
+            }
+            KeyResultAreaDto? keyResultAreaDto = null;
+            if (!string.IsNullOrEmpty(filter.Kra))
+            {
+                var kra = await _keyResulktAreaRepository.GetByIdAsync(int.Parse(filter.Kra), cancellationToken);
+                if (kra != null)
+                    keyResultAreaDto = new KeyResultAreaDto(kra);
+            }
+            var reportDtos = narratives.Select(n =>
+            {
+                return new ReportPGSSummaryNarrativeDto(
+                    n,
+                    deliverables,
+                    filter,
+                    officeDto,
+                    keyResultAreaDto
+                );
+            }).ToList();
+
+            return reportDtos;
+        }
+
+        public async Task<PGSSummaryNarrativeDto?> GetByPeriodIdAsync(int pgsPeriodId, CancellationToken cancellationToken)
+        {
+            var narrative = await _repository.GetByPeriodIdAsync(pgsPeriodId, cancellationToken).ConfigureAwait(false);
+            return narrative == null ? null : new PGSSummaryNarrativeDto(narrative);
+        }
+    
+        public async Task<List<PGSSummaryNarrativeDto>?> GetAllAsync(CancellationToken cancellationToken)
+        {
+            var pgsNarrativeSummary = await _repository.GetAll(cancellationToken).ConfigureAwait(false);
+
+            if (pgsNarrativeSummary == null)
+                return null;
+
+            return pgsNarrativeSummary
+                .Select(x => new PGSSummaryNarrativeDto(x))
+                .ToList();
+        }       
+
+        public async Task SaveOrUpdateAsync<TEntity, TId>(BaseDto<TEntity, TId> dto, CancellationToken cancellationToken) where TEntity : Entity<TId>
+        {
+            var ODto = dto as PGSSummaryNarrativeDto;
+            if (ODto == null) throw new ArgumentException("Invalid DTO type.");
+
+          
+            var pgsSummaryNarrativeDto = ODto!.ToEntity();
+
+            if (pgsSummaryNarrativeDto.Id == 0)
+                _repository.Add(pgsSummaryNarrativeDto);
+            else
+                await _repository.UpdateAsync(pgsSummaryNarrativeDto, pgsSummaryNarrativeDto.Id, cancellationToken).ConfigureAwait(false);
+
+            await _repository.SaveOrUpdateAsync(pgsSummaryNarrativeDto, cancellationToken).ConfigureAwait(false);
+        }
+    }
+}
