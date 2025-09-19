7427aae8fd884a70ff98d11c8b581d762e3f26fd
Wilbur_Pimentel <wilburpimentel1907>
2025-09-19 08:31:39 +0800

    Create PgsDeliverableSummaryNarrativeDto from Application/PgsSummaryNarrativeModule * PgsDeliverableId * PgsPeriod * Office * KeyResultArea * KraDescription * IsDirect * Deliverable * Score * Status * Remarks * ByWhen

---
+++ backend/IMIS/IMIS.Application/PgsSummaryNarrativeModule/PgsDeliverableSummaryNarrativeDto.cs
@@ -0,0 +1,18 @@
+﻿namespace IMIS.Application.PgsSummaryNarrativeModule
+{
+    public class PgsDeliverableSummaryNarrativeDto
+    {
+        public required long PgsDeliverableId { get; set; }
+        public required string PgsPeriod { get; set; }
+        public required string Office { get; set; }
+        public required string KeyResultArea { get; set; }
+        public string? KraDescription { get; set; }
+        public required bool IsDirect { get; set; }
+        public required string Deliverable { get; set; }
+        public required double Score { get; set; }
+        public required string Status { get; set; }
+        public string? Remarks { get; set; }
+        public DateTime ByWhen { get; set; }
+        public PgsDeliverableSummaryNarrativeDto() { }
+    }
+}
