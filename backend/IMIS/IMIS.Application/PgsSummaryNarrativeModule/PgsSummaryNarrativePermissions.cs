ccc89a513979794f537033b54364d0840490a28c
Wilbur_Pimentel <wilburpimentel1907>
2025-09-19 10:14:54 +0800

    - Creating PgsSummaryNarrativePermissions for PgsSummaryNarrative

---
+++ backend/IMIS/IMIS.Application/PgsSummaryNarrativeModule/PgsSummaryNarrativePermissions.cs
@@ -0,0 +1,10 @@
+﻿using Base.Auths.Permissions;
+
+namespace IMIS.Application.PgsSummaryNarrativeModule
+{
+    public class PgsSummaryNarrativePermissions : BaseOperationPermission
+    {
+        public override string ModuleName => "PgsSummaryNarrative";
+        public override string PermissionGroup => PermissionGrouper.Transaction;
+    }
+}
