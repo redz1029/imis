77f9300bac96190386729684ba7ab2f2df4e49af
Wilbur_Pimentel <wilburpimentel1907>
2025-09-19 09:21:03 +0800

    Pgs Deliverable Module IPGSDeliverableRepository  Added *GetPaginatedAsync *GetFilteredAsync

--- backend/IMIS/IMIS.Application/PgsDeliverableModule/IPGSDeliverableRepository.cs
+++ backend/IMIS/IMIS.Application/PgsDeliverableModule/IPGSDeliverableRepository.cs
@@ -1,15 +1,16 @@
 ﻿using Base.Abstractions;
 using Base.Pagination;
 using IMIS.Application.PgsDeliverableModule;
+using IMIS.Application.PgsSummaryNarrativeModule;
 using IMIS.Domain;
 
 namespace IMIS.Application.PgsModule
 {
     public interface IPGSDeliverableRepository : IRepository<PgsDeliverable, long>
     {       
-        Task<EntityPageList<PgsDeliverable, long>> GetPaginatedAsync(int page, int pageSize, CancellationToken cancellationToken);
+        Task<EntityPageList<PgsDeliverable, long>> GetPaginatedAsync(int page, int pageSize, CancellationToken cancellationToken); 
         Task<List<PgsDeliverable>?> GetAll(CancellationToken cancellationToken);       
-        Task<EntityPageList<PgsDeliverable, long>> GetFilteredAsync(PgsDeliverableMonitorFilter filter, CancellationToken cancellationToken);       
+        Task<EntityPageList<PgsDeliverable, long>> GetFilteredAsync(PgsDeliverableMonitorFilter filter, CancellationToken cancellationToken);
         Task<List<int>> GetUserOfficeIdsAsync(string userId, CancellationToken cancellationToken);
     }
 }
