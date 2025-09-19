c4999186e87f6eac8b4bd6373f047605f23a2ed0
Wilbur_Pimentel <wilburpimentel1907>
2025-09-19 08:26:08 +0800

    Create IPGSSummaryNarrativeRepository from PgsSummaryNarrativeModule * GetAll * GetByPeriodIdAsync * GetNarrativesByFilterAsync * GetFilteredDeliverablesAsync

---
+++ backend/IMIS/IMIS.Application/PgsSummaryNarrativeModule/IPGSSummaryNarrativeRepository.cs
@@ -0,0 +1,14 @@
+﻿using Base.Abstractions;
+using IMIS.Domain;
+
+namespace IMIS.Application.PgsSummaryNarrativeModule
+{
+    public interface IPGSSummaryNarrativeRepository : IRepository<PgsSummaryNarrative, int>
+    {
+        Task<IEnumerable<PgsSummaryNarrative>> GetAll(CancellationToken cancellationToken);
+        Task<PgsSummaryNarrative?> GetByPeriodIdAsync(int pgsPeriodId, CancellationToken cancellationToken);
+        Task<List<PgsSummaryNarrative>> GetNarrativesByFilterAsync(PgsDeliverableSummaryNarrativeFilter filter, CancellationToken cancellationToken);
+        Task<List<PgsDeliverable>> GetFilteredDeliverablesAsync(IEnumerable<int> periodIds, PgsDeliverableSummaryNarrativeFilter filter, CancellationToken cancellationToken);
+
+    }
+}
