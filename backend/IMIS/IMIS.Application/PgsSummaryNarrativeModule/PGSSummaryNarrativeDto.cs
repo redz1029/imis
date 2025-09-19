95f8201aee4e056dbc62096717482b855e57fdc6
Wilbur_Pimentel <wilburpimentel1907>
2025-09-19 08:29:30 +0800

    Create PGSSummaryNarrativeDto from Application/PgsSummaryNarrativeModule * Findings * Recommendation * Conclusion * PgsPeriodId

---
+++ backend/IMIS/IMIS.Application/PgsSummaryNarrativeModule/PGSSummaryNarrativeDto.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Base.Primitives;
+using IMIS.Domain;
+
+namespace IMIS.Application.PgsSummaryNarrativeModule
+{
+    public class PGSSummaryNarrativeDto : BaseDto<PgsSummaryNarrative, int>
+    {
+        public required string Findings { get; set; }
+        public required string Recommendation { get; set; }
+        public required string Conclusion { get; set; }
+        public int? PgsPeriodId { get; set; }
+       
+        public PGSSummaryNarrativeDto()
+        {
+        }
+
+        [SetsRequiredMembers]
+        public PGSSummaryNarrativeDto(PgsSummaryNarrative pgsSummaryNarrative)
+        {
+            this.Id = pgsSummaryNarrative.Id;
+            this.Findings = pgsSummaryNarrative.Findings;
+            this.Recommendation = pgsSummaryNarrative.Recommendation;
+            this.Conclusion = pgsSummaryNarrative.Conclusion;
+            this.PgsPeriodId = pgsSummaryNarrative.PgsPeriodId;
+        }
+
+        public override PgsSummaryNarrative ToEntity()
+        {
+            return new PgsSummaryNarrative()
+            {
+                Id = Id,
+                Findings = Findings,
+                Recommendation = Recommendation,
+                Conclusion = Conclusion,
+                PgsPeriodId = PgsPeriodId,
+            };
+        }
+    }
+}
