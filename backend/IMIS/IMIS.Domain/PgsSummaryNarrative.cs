8bf13fe110550c4792ee03b7b0a8e3c3d613cc8f
Wilbur_Pimentel <wilburpimentel1907>
2025-09-19 08:43:14 +0800

    Create PgsSummaryNarrative from Domain * Findings * Recommendation * Conclusion * PgsPeriodId

---
+++ backend/IMIS/IMIS.Domain/PgsSummaryNarrative.cs
@@ -0,0 +1,14 @@
+﻿using Base.Primitives;
+
+namespace IMIS.Domain
+{
+    public class PgsSummaryNarrative : Entity<int>
+    {
+        public required string Findings { get; set; }
+        public required string Recommendation { get; set; }
+        public required string Conclusion { get; set; }
+        public int? PgsPeriodId { get; set; }
+        public PgsPeriod? PgsPeriod { get; set; }
+
+    }
+}
