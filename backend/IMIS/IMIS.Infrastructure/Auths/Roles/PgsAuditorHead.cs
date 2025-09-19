5161bc88494b16a2e24b172c1c3f39d218b8260c
Wilbur_Pimentel <wilburpimentel1907>
2025-09-19 11:33:49 +0800

    - Creating Role for Pgs Auditor Head

---
+++ backend/IMIS/IMIS.Infrastructure/Auths/Roles/PgsAuditorHead.cs
@@ -0,0 +1,9 @@
+﻿using Base.Auths.Roles;
+
+namespace IMIS.Infrastructure.Auths.Roles
+{
+    public class PgsAuditorHead : BaseRole
+    {
+        public override string Name => "PGS Auditor Head";
+    }
+}
