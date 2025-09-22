3baa082e3d7aba05d1720d6768d90d18d3251c53
Earlene Mae Rama <earlmaerama@gmail.com>
2025-09-22 12:02:55 +0800

    Create pdf summary

---
+++ frontend/imis/lib/reports/pages/view_pdf_summary.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:imis/constant/constant.dart';
+import 'package:imis/utils/api_endpoint.dart';
+import '../../widgets/universal_webview_web.dart';
+
+class ViewPdfSummary extends StatelessWidget {
+  final String pgsPeriodId;
+
+  const ViewPdfSummary({super.key, required this.pgsPeriodId});
+
+  @override
+  Widget build(BuildContext context) {
+    final pdfUrl =
+        "${ApiEndpoint().pgsSummaryNarrativeListReport}?PgsPeriodId=$pgsPeriodId&Page=1&PageSize=25";
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Summary Narrative Report"),
+        backgroundColor: mainBgColor,
+      ),
+      body: UniversalWebView(url: pdfUrl),
+    );
+  }
+}
