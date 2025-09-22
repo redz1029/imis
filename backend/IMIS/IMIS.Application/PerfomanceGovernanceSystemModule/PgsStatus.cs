4c0e9ef13492fc3b0798bcb54347b0f779848d71
Wilbur_Pimentel <wilburpimentel1907>
2025-09-22 11:07:47 +0800

    Added Approved By in PgsStatus

--- backend/IMIS/IMIS.Application/PerfomanceGovernanceSystemModule/PgsStatus.cs
+++ backend/IMIS/IMIS.Application/PerfomanceGovernanceSystemModule/PgsStatus.cs
@@ -5,8 +5,9 @@
         public const string Prepared = "Prepared";
         public const string Pending = "Pending";
         public const string Approved = "Approved";
+        public const string ApprovedBy = "Approved By";
         public const string Disapproved = "Disapproved";
-        public const string PreparedBy = "Prepared By";
+        public const string PreparedBy = "Prepared By";       
         public static string OfficeHead { get; set; } = "Office Head";
     }
 }
