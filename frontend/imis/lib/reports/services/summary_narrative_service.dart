78fe2e18ad0207028a6735bbb600e4abf3c46746
Earlene Mae Rama <earlmaerama@gmail.com>
2025-09-22 12:05:56 +0800

    Add getPeriodDate on summary narrative serice

--- frontend/imis/lib/reports/services/summary_narrative_service.dart
+++ frontend/imis/lib/reports/services/summary_narrative_service.dart
@@ -1,12 +1,30 @@
 import 'package:dio/dio.dart';
+import 'package:imis/performance_governance_system/pgs_period/models/pgs_period.dart';
 import 'package:imis/reports/models/pgs_summary_narrative.dart';
 import 'package:imis/utils/api_endpoint.dart';
 import 'package:imis/utils/http_util.dart';
+import 'package:imis/utils/page_list.dart';
+import 'package:imis/utils/pagination_util.dart';
 
 class SummaryNarrativeService {
   final Dio dio;
 
   SummaryNarrativeService(this.dio);
+
+  Future<PageList<PgsSummaryNarrative>> getSummaryNarrative({
+    int page = 1,
+    int pageSize = 15,
+    String? searchQuery,
+  }) async {
+    final paginationUtil = PaginationUtil(dio);
+    return await paginationUtil.fetchPaginatedData<PgsSummaryNarrative>(
+      endpoint: ApiEndpoint().summaryNarrative,
+      page: page,
+      pageSize: pageSize,
+      searchQuery: searchQuery,
+      fromJson: (json) => PgsSummaryNarrative.fromJson(json),
+    );
+  }
 
   Future<void> addSummaryNarrative(
     PgsSummaryNarrative pgsSummaryNarrative,
@@ -20,6 +38,21 @@ class SummaryNarrativeService {
     );
     if (response.statusCode != 200 && response.statusCode != 201) {
       throw Exception('Failed to create summary narrative');
+    }
+  }
+
+  Future<PgsPeriod> getPeriodDates(int periodId) async {
+    final url = ApiEndpoint().pgsperiod;
+    final response = await AuthenticatedRequest.get(
+      dio,
+      url,
+    ); // Adjust API path accordingly
+
+    if (response.statusCode == 200) {
+      final data = response.data;
+      return PgsPeriod(0, false, DateTime.now(), DateTime.now(), 'remarks');
+    } else {
+      throw Exception('Failed to fetch period dates');
     }
   }
 
