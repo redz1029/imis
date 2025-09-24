6274e4e7c0e518ece663ca781fc9b577c725dd12
Wilbur_Pimentel <wilburpimentel1907>
2025-09-24 11:30:39 +0800

    Create PgsDeliverableAccomplishmentPermission

---
+++ backend/IMIS/IMIS.Application/PgsDeliverableAccomplishmentModule/PgsDeliverableAccomplishmentPermission.cs
@@ -0,0 +1,10 @@
+﻿using Base.Auths.Permissions;
+
+namespace IMIS.Application.PgsDeliverableAccomplishmentModule
+{
+    public class PgsDeliverableAccomplishmentPermission : BaseOperationPermission
+    {
+        public override string ModuleName => "PgsDeliverableAccomplishment";
+        public override string PermissionGroup => PermissionGrouper.Setting;
+    }
+}
