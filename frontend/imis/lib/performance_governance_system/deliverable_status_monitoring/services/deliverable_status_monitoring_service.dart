0060530b26ad1868d5204758278017b1f90c4991
Earlene Mae Rama <earlmaerama@gmail.com>
2025-09-30 10:53:46 +0800

    - #917: Add Update/Save accomplishment, fetchAccomplishments service

--- frontend/imis/lib/performance_governance_system/deliverable_status_monitoring/services/deliverable_status_monitoring_service.dart
+++ frontend/imis/lib/performance_governance_system/deliverable_status_monitoring/services/deliverable_status_monitoring_service.dart
@@ -2,6 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:imis/office/models/office.dart';
 import 'package:imis/utils/api_endpoint.dart';
 import 'package:imis/utils/http_util.dart';
+import '../models/pgs_deliverable_accomplishment.dart';
 
 class DeliverableStatusMonitoringService {
   final Dio dio;
@@ -24,13 +25,38 @@ class DeliverableStatusMonitoringService {
     }
   }
 
-  Future<void> saveAccomplishment(List<Map<String, dynamic>> data) async {
-    final url = ApiEndpoint().pgsDeliverableAccomplishment;
+  Future<void> saveAccomplishment(FormData formData) async {
+    final id =
+        formData.fields
+            .firstWhere((f) => f.key == 'id', orElse: () => MapEntry('id', ''))
+            .value;
+    final url =
+        ApiEndpoint().pgsDeliverableAccomplishment +
+        (id.isNotEmpty ? '/$id' : '');
 
-    final response = await AuthenticatedRequest.post(dio, url, data: data);
+    final response =
+        id.isNotEmpty
+            ? await AuthenticatedRequest.put(dio, url, data: formData)
+            : await AuthenticatedRequest.post(dio, url, data: formData);
 
     if (response.statusCode != 200 && response.statusCode != 201) {
       throw Exception('Failed to create/update PGS accomplishment');
+    }
+  }
+
+  Future<List<PgsDeliverableAccomplishment>> fetchAccomplishments(
+    int deliverableId,
+  ) async {
+    final url = "${ApiEndpoint.baseUrl}/$deliverableId";
+    final response = await AuthenticatedRequest.get(dio, url);
+
+    if (response.statusCode == 200) {
+      final data = response.data as List;
+      return data
+          .map((json) => PgsDeliverableAccomplishment.fromJson(json))
+          .toList();
+    } else {
+      throw Exception('Failed to fetch PGS accomplishments');
     }
   }
 }
