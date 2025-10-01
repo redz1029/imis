47576864e91adff77bed469c99b1151f8cdc4b31
Wilbur_Pimentel <wilburpimentel1907>
2025-10-01 14:17:17 +0800

    #944: Create Team Configuration from SeedConfiguration

---
+++ backend/IMIS/IMIS.Persistence/SeedConfigurations/TeamConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using IMIS.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace IMIS.Persistence.SeedConfigurations
+{
+    public class TeamConfiguration : IEntityTypeConfiguration<Team>
+    {
+        public void Configure(EntityTypeBuilder<Team> builder)
+        {
+            builder.HasData(
+            new Team { Id = 1, Name = "Team 1", IsActive = true, IsDeleted = false },
+            new Team { Id = 2, Name = "Team 2", IsActive = true, IsDeleted = false },
+            new Team { Id = 3, Name = "Team 3", IsActive = true, IsDeleted = false },
+            new Team { Id = 4, Name = "Team 4", IsActive = true, IsDeleted = false },
+            new Team { Id = 5, Name = "Team 5", IsActive = true, IsDeleted = false },
+            new Team { Id = 6, Name = "Team 6", IsActive = true, IsDeleted = false },
+            new Team { Id = 7, Name = "Team 7", IsActive = true, IsDeleted = false },
+            new Team { Id = 8, Name = "Team 8", IsActive = true, IsDeleted = false },
+            new Team { Id = 9, Name = "Team 9", IsActive = true, IsDeleted = false },
+            new Team { Id = 10, Name = "Team 10", IsActive = true, IsDeleted = false },
+            new Team { Id = 11, Name = "Team 11", IsActive = true, IsDeleted = false },
+            new Team { Id = 12, Name = "Team 12", IsActive = true, IsDeleted = false },
+            new Team { Id = 13, Name = "Team 13", IsActive = true, IsDeleted = false },
+            new Team { Id = 14, Name = "Team 14", IsActive = true, IsDeleted = false });
+        }
+    }
+}
