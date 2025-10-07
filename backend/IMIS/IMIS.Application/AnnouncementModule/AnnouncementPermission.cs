439c7d1921bd62214bb445392bdc6ef51061b38c
Wilbur_Pimentel <wilburpimentel1907>
2025-10-07 13:21:59 +0800

    #978: Create Permission for Announcement

---
+++ backend/IMIS/IMIS.Application/AnnouncementModule/AnnouncementPermission.cs
@@ -0,0 +1,10 @@
+﻿using Base.Auths.Permissions;
+
+namespace IMIS.Application.AnnouncementModule
+{
+    public class AnnouncementPermission : BaseOperationPermission
+    {
+        public override string ModuleName => "Announcement";
+        public override string PermissionGroup => PermissionGrouper.Setting;
+    }
+}
