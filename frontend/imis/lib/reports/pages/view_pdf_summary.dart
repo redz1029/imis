c29a1a92b0805dc4ee239df5bb03bc3ede4b8f89
Earlene Mae Rama <earlmaerama@gmail.com>
2025-10-07 16:37:35 +0800

    Remove unused codes and imports

--- frontend/imis/lib/reports/pages/view_pdf_summary.dart
+++ frontend/imis/lib/reports/pages/view_pdf_summary.dart
@@ -1,32 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:imis/constant/constant.dart';
-// import 'package:imis/utils/api_endpoint.dart';
-// import '../../widgets/universal_webview_web.dart';
-
-// class ViewPdfSummary extends StatelessWidget {
-//   final String pgsPeriodId;
-//   final String officeId;
-//   const ViewPdfSummary({
-//     super.key,
-//     required this.pgsPeriodId,
-//     required this.officeId,
-//   });
-
-//   @override
-//   Widget build(BuildContext context) {
-//     final pdfUrl =
-//         "${ApiEndpoint().pgsSummaryNarrativeListReport}?PgsPeriodId=$pgsPeriodId&Page=1&PageSize=25";
-
-//     return Scaffold(
-//       backgroundColor: mainBgColor,
-//       appBar: AppBar(
-//         title: const Text("Summary Narrative Report"),
-//         backgroundColor: mainBgColor,
-//       ),
-//       body: UniversalWebView(url: pdfUrl),
-//     );
-//   }
-// }
 import 'package:flutter/material.dart';
 import 'package:imis/constant/constant.dart';
 import 'package:imis/utils/api_endpoint.dart';
