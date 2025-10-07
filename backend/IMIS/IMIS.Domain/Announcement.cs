733193a9e5f08ce906f1d55f8f5dd10969e2647f
Wilbur_Pimentel <wilburpimentel1907>
2025-10-07 13:10:59 +0800

    #977: Created Announcement Domain

---
+++ backend/IMIS/IMIS.Domain/Announcement.cs
@@ -0,0 +1,13 @@
+﻿using Base.Primitives;
+
+namespace IMIS.Domain
+{
+    public class Announcement : Entity<int>
+    {
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public DateTime FromDate { get; set; }
+        public DateTime ToDate { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
