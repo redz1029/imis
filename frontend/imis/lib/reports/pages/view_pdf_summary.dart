d44da9a491c399c008851fec559aaed76427623f
Earlene Mae Rama <earlmaerama@gmail.com>
2025-10-09 15:15:18 +0800

    - #919: Make pdf summary dynamic whether to fetch officeId or not

--- frontend/imis/lib/reports/pages/view_pdf_summary.dart
+++ frontend/imis/lib/reports/pages/view_pdf_summary.dart
@@ -5,18 +5,20 @@ import '../../widgets/universal_webview_web.dart';
 
 class ViewPdfSummary extends StatelessWidget {
   final String pgsPeriodId;
-  final String officeId;
+  final String? officeId;
 
-  const ViewPdfSummary({
-    super.key,
-    required this.pgsPeriodId,
-    required this.officeId,
-  });
+  const ViewPdfSummary({super.key, required this.pgsPeriodId, this.officeId});
 
   @override
   Widget build(BuildContext context) {
-    final pdfUrl =
-        "${ApiEndpoint().pgsSummaryNarrativeListReport}?PgsPeriodId=$pgsPeriodId&OfficeId=$officeId&Page=1&PageSize=25";
+    String pdfUrl =
+        "${ApiEndpoint().pgsSummaryNarrativeListReport}?PgsPeriodId=$pgsPeriodId";
+
+    if (officeId != null && officeId!.isNotEmpty) {
+      pdfUrl += "&OfficeId=$officeId";
+    }
+
+    pdfUrl += "&Page=1&PageSize=25";
 
     return Scaffold(
       backgroundColor: mainBgColor,
