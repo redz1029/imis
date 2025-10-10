ca30f1819c4c54863a2367c6889e77b90ab03958
Wilbur_Pimentel <wilburpimentel1907>
2025-10-10 16:00:37 +0800

    #1001: Auditable Office DTO:  - Added SetsRequiredMembers and constructor from AuditableOfficesDto

--- backend/IMIS/IMIS.Application/AuditableOfficesModule/AuditableOfficesDto.cs
+++ backend/IMIS/IMIS.Application/AuditableOfficesModule/AuditableOfficesDto.cs
@@ -1,4 +1,5 @@
-﻿using IMIS.Domain;
+﻿using System.Diagnostics.CodeAnalysis;
+using IMIS.Domain;
 
 namespace IMIS.Application.AuditableOfficesModule
 {
@@ -6,13 +7,21 @@ namespace IMIS.Application.AuditableOfficesModule
     {
         public required int AuditScheduleId { get; set; }       
         public required int OfficeId { get; set; }
-      
+
+        public AuditableOfficesDto() { }
+        [SetsRequiredMembers]
+        public AuditableOfficesDto(AuditableOffices auditableOffices)
+        {
+            this.AuditScheduleId = auditableOffices.AuditScheduleId;
+            this.OfficeId = auditableOffices.OfficeId;         
+        }
+
         public AuditableOffices ToEntity()
         {
             return new AuditableOffices
             {                
                 AuditScheduleId = AuditScheduleId,
-                OfficeId = OfficeId
+                OfficeId = OfficeId,                   
             };
         }
     }
