0aa8737f5f5ad71b9e750d9e1be39e98a685180f
Wilbur_Pimentel <wilburpimentel1907>
2025-10-16 09:13:25 +0800

    #1009: Created a new Break Through Scoring Permission

---
+++ backend/IMIS/IMIS.Application/BreakThroughScoringModule/BreakThroughScoringPermission.cs
@@ -0,0 +1,10 @@
+﻿using Base.Auths.Permissions;
+
+namespace IMIS.Application.BreakThroughScoringModule
+{
+    public class BreakThroughScoringPermission : BaseOperationPermission
+    {
+        public override string ModuleName => "BreakThroughScoring";
+        public override string PermissionGroup => PermissionGrouper.Transaction;
+    }
+}
