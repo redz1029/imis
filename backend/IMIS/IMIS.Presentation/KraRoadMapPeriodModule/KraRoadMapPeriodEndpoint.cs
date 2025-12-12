5f983ff2485b823a7190c2af99d6744a830df915
Wilbur_Pimentel <wilburpimentel1907>
2025-12-12 13:59:54 +0800

    #1084: KRA Period - remove non used imports

--- backend/IMIS/IMIS.Presentation/KraRoadMapPeriodModule/KraRoadMapPeriodEndpoint.cs
+++ backend/IMIS/IMIS.Presentation/KraRoadMapPeriodModule/KraRoadMapPeriodEndpoint.cs
@@ -1,8 +1,6 @@
 ﻿using Base.Auths.Permissions;
 using Carter;
 using IMIS.Application.KraRoadMapPeriodModule;
-using IMIS.Application.PgsKeyResultAreaModule;
-using IMIS.Application.PgsKraModule;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
