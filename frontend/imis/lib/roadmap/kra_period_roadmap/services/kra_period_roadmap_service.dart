19a8f632a09da40f7faacb4caa97a1de16c7416a
Earlene Mae Rama <earlmaerama@gmail.com>
2025-12-12 09:36:36 +0800

    - #1087: Create KRA Roadmap period service

---
+++ frontend/imis/lib/roadmap/kra_period_roadmap/services/kra_period_roadmap_service.dart
@@ -0,0 +1,44 @@
+import 'package:dio/dio.dart';
+import 'package:imis/roadmap/kra_period_roadmap/models/kra_roadmap_period.dart';
+import 'package:imis/utils/api_endpoint.dart';
+import 'package:imis/utils/http_util.dart';
+import 'package:imis/utils/page_list.dart';
+import 'package:imis/utils/pagination_util.dart';
+
+class KraPeriodRoadmapService {
+  final Dio dio;
+
+  KraPeriodRoadmapService(this.dio);
+  Future<PageList<KraRoadmapPeriod>> getPgsPeriod({
+    int page = 1,
+    int pageSize = 15,
+    String? searchQuery,
+  }) async {
+    final paginationUtil = PaginationUtil(dio);
+    return await paginationUtil.fetchPaginatedData(
+      endpoint: ApiEndpoint().kraRoadMapPeriod,
+      page: page,
+      pageSize: pageSize,
+      searchQuery: searchQuery,
+      fromJson: (json) => KraRoadmapPeriod.fromJson(json),
+    );
+  }
+
+  Future<void> createOrUpdatePgsPeriod(KraRoadmapPeriod period) async {
+    var url = ApiEndpoint().kraRoadMapPeriod;
+    final Map<String, dynamic> requestData = period.toJson();
+    final response = await AuthenticatedRequest.post(
+      dio,
+      url,
+      data: requestData,
+    );
+    if (response.statusCode != 200 && response.statusCode != 201) {
+      throw Exception('Failed to create kraRoadMapPeriod');
+    }
+  }
+
+  Future<void> deletePeriod(String period) async {
+    final url = '${ApiEndpoint().kraRoadMapPeriod}/$period';
+    await AuthenticatedRequest.delete(dio, url);
+  }
+}
