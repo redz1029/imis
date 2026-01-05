842b3b033ea6dbe2e9edea96f445d1f593f97449
Earlene Mae Rama <earlmaerama@gmail.com>
2026-01-05 11:26:25 +0800

    - #1087: Remove updateRoadmap on service

--- frontend/imis/lib/roadmap/services/roadmap_service.dart
+++ frontend/imis/lib/roadmap/services/roadmap_service.dart
@@ -40,24 +40,6 @@ class RoadmapService {
     }
   }
 
-  Future<void> updateRoadmap(Roadmap roadmap) async {
-    if (roadmap.id == 0) {
-      throw Exception('Roadmap ID is required for update');
-    }
-
-    final url = '${ApiEndpoint().kraRoadMap}/${roadmap.id}';
-
-    final response = await AuthenticatedRequest.put(
-      dio,
-      url,
-      data: roadmap.toJson(),
-    );
-
-    if (response.statusCode != 200 && response.statusCode != 204) {
-      throw Exception('Failed to update roadmap');
-    }
-  }
-
   Future<void> deleteRoadmap(String roadMap) async {
     final url = '${ApiEndpoint().kraRoadMap}/$roadMap';
     await AuthenticatedRequest.delete(dio, url);
