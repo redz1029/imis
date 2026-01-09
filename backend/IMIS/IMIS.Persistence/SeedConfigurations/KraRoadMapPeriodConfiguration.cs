0eb70bc4fca86b4be6d19e9283df9a565cbdf32f
Wilbur_Pimentel <wilburpimentel1907>
2026-01-09 19:29:39 +0800

    #1188: Seed Kra Roadmap Period

---
+++ backend/IMIS/IMIS.Persistence/SeedConfigurations/KraRoadMapPeriodConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using IMIS.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace IMIS.Persistence.SeedConfigurations
+{
+    public class KraRoadMapPeriodConfiguration : IEntityTypeConfiguration<KraRoadMapPeriod>
+    {
+        public void Configure(EntityTypeBuilder<KraRoadMapPeriod> builder)
+        {
+            builder.HasData(
+                new KraRoadMapPeriod
+                {
+                    Id = 1,
+                    StartYear = new DateOnly(2026, 1, 1),
+                    EndYear = new DateOnly(2030, 12, 31),
+                    IsDeleted = false
+                }
+            );
+        }
+    }
+}
