88c5964dcfc7dd6ea361a63107319e6b1b7dfdf3
Kit <aviciivilloso@gmail.com>
2026-02-02 10:54:39 +0800

    Removed training related modules

--- backend/IMIS/IMIS.Application/CharacterModule/ICharacterNoteRepository.cs
+++
@@ -1,9 +0,0 @@
-using Base.Abstractions;
-using IMIS.Domain;
-
-namespace IMIS.Application.CharacterModule
-{
-    public interface ICharacterNoteRepository : IRepository<CharacterNote, long>
-    {
-    }
-}
