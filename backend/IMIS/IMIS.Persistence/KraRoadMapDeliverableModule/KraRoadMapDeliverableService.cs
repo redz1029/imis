0b466febad861882be3249ab6608492b9af071bf
Wilbur_Pimentel <wilburpimentel1907>
2026-06-01 07:56:18 +0800

    #1088: KRA Road Map Implemented GetByRoadMapIdAsync, GetRoadMapPeriodsAsync and  GetDeliverablesByIdsAsync method

--- backend/IMIS/IMIS.Persistence/KraRoadMapDeliverableModule/KraRoadMapDeliverableService.cs
+++ backend/IMIS/IMIS.Persistence/KraRoadMapDeliverableModule/KraRoadMapDeliverableService.cs
@@ -1,5 +1,6 @@
 ﻿using Base.Primitives;
 using IMIS.Application.KraRoadMapDeliverableModule;
+using IMIS.Application.KraRoadMapModule;
 
 namespace IMIS.Persistence.KraRoadMapDeliverableModule
 {
@@ -9,6 +10,35 @@ namespace IMIS.Persistence.KraRoadMapDeliverableModule
         public KraRoadMapDeliverableService(IKraRoadMapDeliverableRepository repository)
         {
             _repository = repository;
+        }
+     
+        public async Task<List<FilterKraPeriodKraDeliverableDto>> GetByRoadMapIdAsync(long kraRoadMapId, int year,
+        CancellationToken cancellationToken)
+        {
+            var deliverables = await _repository.GetByRoadMapIdAsync(kraRoadMapId, year, cancellationToken);
+
+            if (!deliverables.Any())
+                return new List<FilterKraPeriodKraDeliverableDto>();
+
+            var roadmapPeriods = await _repository.GetRoadMapPeriodsAsync(deliverables, cancellationToken);
+
+            var grouped = deliverables
+                .GroupBy(d =>
+                    roadmapPeriods.First(r =>
+                        r.KraRoadMapId == d.KraRoadMapId))
+                .Select(g => new FilterKraPeriodKraDeliverableDto
+                {
+                    StartDate = DateOnly.FromDateTime(g.Key.StartYear),
+                    EndDate = DateOnly.FromDateTime(g.Key.EndYear),
+
+                    Deliverables = g
+                        .Select(d => new KraRoadMapDeliverableDto(d))
+                        .ToList()
+                })
+                .OrderBy(x => x.StartDate)
+                .ToList();
+
+            return grouped;
         }
 
         public async Task SaveOrUpdateAsync<TEntity, TId>(BaseDto<TEntity, TId> dto, CancellationToken cancellationToken) where TEntity : Entity<TId>
