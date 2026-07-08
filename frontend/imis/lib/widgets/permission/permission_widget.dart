dac2d7e981ce4ad87eb6bc6177af19b26855d759
Earlene Mae Rama <earlmaerama@gmail.com>
2026-07-08 13:14:06 +0800

    - #612: Refractor Permission Widget

--- frontend/imis/lib/widgets/permission/permission_widget.dart
+++ frontend/imis/lib/widgets/permission/permission_widget.dart
@@ -1,3 +1,39 @@
+// import 'package:flutter/material.dart';
+// import 'package:imis/utils/permission_service.dart';
+
+// final permissionService = PermissionService();
+
+// class PermissionWidget extends StatelessWidget {
+//   final String? permission;
+//   final List<String>? allowedRoles;
+//   final Widget child;
+//   final bool ignorePermission;
+
+//   const PermissionWidget({
+//     super.key,
+//     this.permission,
+//     this.allowedRoles,
+//     required this.child,
+//     this.ignorePermission = false,
+//   });
+
+//   @override
+//   Widget build(BuildContext context) {
+//     if (ignorePermission) return child;
+
+//     final hasPermission =
+//         permission != null
+//             ? permissionService.hasPermission(
+//               permission!,
+//               allowedRoles: allowedRoles,
+//             )
+//             : (allowedRoles != null
+//                 ? allowedRoles!.contains(permissionService.currentRole)
+//                 : true);
+
+//     return hasPermission ? child : const SizedBox.shrink();
+//   }
+// }
 import 'package:flutter/material.dart';
 import 'package:imis/utils/permission_service.dart';
 
@@ -7,6 +43,7 @@ class PermissionWidget extends StatelessWidget {
   final String? permission;
   final List<String>? allowedRoles;
   final Widget child;
+  final Widget? fallback;
   final bool ignorePermission;
 
   const PermissionWidget({
@@ -14,6 +51,7 @@ class PermissionWidget extends StatelessWidget {
     this.permission,
     this.allowedRoles,
     required this.child,
+    this.fallback,
     this.ignorePermission = false,
   });
 
@@ -31,6 +69,6 @@ class PermissionWidget extends StatelessWidget {
                 ? allowedRoles!.contains(permissionService.currentRole)
                 : true);
 
-    return hasPermission ? child : const SizedBox.shrink();
+    return hasPermission ? child : (fallback ?? const SizedBox.shrink());
   }
 }
