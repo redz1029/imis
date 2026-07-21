24b22f8fcf5601e502b90f8c964f44fa99c269b5
Wilbur_Pimentel <wilburpimentel1907>
2026-07-21 10:52:59 +0800

    #1686: Create module for setting permssion

---
+++ backend/IMIS/IMIS.Application/PgsPermissionSettings/CreateViewOperationReviewProtocolPermission.cs
@@ -0,0 +1,10 @@
+﻿using Base.Auths.Permissions;
+
+namespace IMIS.Application.PgsPermissionSettings
+{
+    public class CreateViewOperationReviewProtocolPermission : BaseOperationPermission
+    {
+        public override string ModuleName => "CreateViewOperationReviewProtocolSetting";
+        public override string PermissionGroup => PermissionGrouper.Transaction;
+    }
+}
---
+++ backend/IMIS/IMIS.Application/PgsPermissionSettings/DeliverableStatusMonitoringPermission.cs
@@ -0,0 +1,10 @@
+﻿using Base.Auths.Permissions;
+
+namespace IMIS.Application.PgsPermissionSettings
+{
+    public class DeliverableStatusMonitoringPermission : BaseOperationPermission
+    {
+        public override string ModuleName => "DeliverableStatusMonitoringSetting";
+        public override string PermissionGroup => PermissionGrouper.Transaction;
+    }
+}
