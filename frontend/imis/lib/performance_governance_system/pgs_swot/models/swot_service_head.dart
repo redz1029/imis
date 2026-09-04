3fce4615d3aa3b9295865344ca0f6f3f137065c6
Earlene Mae Rama <earlmaerama@gmail.com>
2026-09-04 14:43:34 +0800

    Create SWOT Service Head models

---
+++ frontend/imis/lib/performance_governance_system/pgs_swot/models/swot_service_head.dart
@@ -0,0 +1,63 @@
+import 'package:imis/performance_governance_system/pgs_swot/models/swot_ot_deliverable.dart';
+import 'package:imis/performance_governance_system/pgs_swot/models/swot_sw_deliverable.dart';
+import 'package:imis/utils/date_time_converter.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'swot_service_head.g.dart';
+
+@JsonSerializable()
+class Swot {
+  int? id;
+
+  bool? isDeleted;
+
+  String? rowVersion;
+
+  int? departmentId;
+
+  String? departmentName;
+
+  String? objectiveStatement;
+
+  String? departmentChairUserId;
+
+  String? departmentChairUserFullName;
+
+  String? qmrUserId;
+
+  String? qmrUserFullName;
+
+  String? serviceHeadUserId;
+
+  String? serviceHeadUserFullName;
+
+  @JsonKey()
+  @DateTimeConverter()
+  DateTime? postingDate;
+
+  List<SwotSwDeliverable>? swotAnalysisSWDeliverables;
+
+  List<SwotOtDeliverable>? swotAnalysisOTDeliverables;
+
+  Swot({
+    this.id,
+    this.isDeleted,
+    this.rowVersion,
+    this.departmentId,
+    this.departmentName,
+    this.objectiveStatement,
+    this.departmentChairUserId,
+    this.departmentChairUserFullName,
+    this.qmrUserId,
+    this.qmrUserFullName,
+    this.serviceHeadUserId,
+    this.serviceHeadUserFullName,
+    this.postingDate,
+    this.swotAnalysisSWDeliverables,
+    this.swotAnalysisOTDeliverables,
+  });
+
+  factory Swot.fromJson(Map<String, dynamic> json) => _$SwotFromJson(json);
+
+  Map<String, dynamic> toJson() => _$SwotToJson(this);
+}
---
+++ frontend/imis/lib/performance_governance_system/pgs_swot/models/swot_service_head.g.dart
@@ -0,0 +1,65 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'swot_service_head.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Swot _$SwotFromJson(Map<String, dynamic> json) => Swot(
+  id: (json['id'] as num?)?.toInt(),
+  isDeleted: json['isDeleted'] as bool?,
+  rowVersion: json['rowVersion'] as String?,
+  departmentId: (json['departmentId'] as num?)?.toInt(),
+  departmentName: json['departmentName'] as String?,
+  objectiveStatement: json['objectiveStatement'] as String?,
+  departmentChairUserId: json['departmentChairUserId'] as String?,
+  departmentChairUserFullName: json['departmentChairUserFullName'] as String?,
+  qmrUserId: json['qmrUserId'] as String?,
+  qmrUserFullName: json['qmrUserFullName'] as String?,
+  serviceHeadUserId: json['serviceHeadUserId'] as String?,
+  serviceHeadUserFullName: json['serviceHeadUserFullName'] as String?,
+  postingDate: _$JsonConverterFromJson<String, DateTime>(
+    json['postingDate'],
+    const DateTimeConverter().fromJson,
+  ),
+  swotAnalysisSWDeliverables:
+      (json['swotAnalysisSWDeliverables'] as List<dynamic>?)
+          ?.map((e) => SwotSwDeliverable.fromJson(e as Map<String, dynamic>))
+          .toList(),
+  swotAnalysisOTDeliverables:
+      (json['swotAnalysisOTDeliverables'] as List<dynamic>?)
+          ?.map((e) => SwotOtDeliverable.fromJson(e as Map<String, dynamic>))
+          .toList(),
+);
+
+Map<String, dynamic> _$SwotToJson(Swot instance) => <String, dynamic>{
+  'id': instance.id,
+  'isDeleted': instance.isDeleted,
+  'rowVersion': instance.rowVersion,
+  'departmentId': instance.departmentId,
+  'departmentName': instance.departmentName,
+  'objectiveStatement': instance.objectiveStatement,
+  'departmentChairUserId': instance.departmentChairUserId,
+  'departmentChairUserFullName': instance.departmentChairUserFullName,
+  'qmrUserId': instance.qmrUserId,
+  'qmrUserFullName': instance.qmrUserFullName,
+  'serviceHeadUserId': instance.serviceHeadUserId,
+  'serviceHeadUserFullName': instance.serviceHeadUserFullName,
+  'postingDate': _$JsonConverterToJson<String, DateTime>(
+    instance.postingDate,
+    const DateTimeConverter().toJson,
+  ),
+  'swotAnalysisSWDeliverables': instance.swotAnalysisSWDeliverables,
+  'swotAnalysisOTDeliverables': instance.swotAnalysisOTDeliverables,
+};
+
+Value? _$JsonConverterFromJson<Json, Value>(
+  Object? json,
+  Value? Function(Json json) fromJson,
+) => json == null ? null : fromJson(json as Json);
+
+Json? _$JsonConverterToJson<Json, Value>(
+  Value? value,
+  Json? Function(Value value) toJson,
+) => value == null ? null : toJson(value);
